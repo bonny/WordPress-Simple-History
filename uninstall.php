6d7f19fc0ee0137dfe8ff44cbb70d2540aeb57fd
Pär Thernström <par@earthpeople.se>
2013-06-19 20:46:05 +0200

    uninstall

--- uninstall.php
+++ uninstall.php
@@ -3,7 +3,32 @@
  * File that is run during plugin uninstall (not just de-activate)
  */
 
-//if uninstall not called from WordPress exit
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
+// If uninstall not called from WordPress exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit ();
 
+/*
+Go on with uninstall actions:
+ - Remove our database table
+ - Remove options:
+*/
+
+// Remove options
+$arr_options = array(
+ 	"simple_history_pager_size",
+ 	"simple_history_db_version",
+ 	"simple_history_rss_secret",
+ 	"simple_history_show_on_dashboard",
+ 	"simple_history_show_as_page"
+);
+
+foreach ($arr_options as $one_option) {
+	delete_option( $one_option );
+}
+
+// Remove database table
+global $wpdb;
+$table_name = $wpdb->prefix . "simple_history";
+$wpdb->query("DROP TABLE IF EXISTS $table_name");
+
+// And we are done. Simple History is ...  history.
