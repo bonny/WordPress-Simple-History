0ac2572d24ec1eee8420ecd7f46cdab701bffdde
Pär Thernström <par.thernstrom@gmail.com>
2014-07-19 00:06:29 +0200

    html fix

--- templates/settings-test.php
+++ templates/settings-test.php
@@ -18,14 +18,20 @@ $total_num_rows = $wpdb->get_var("select count(*) FROM {$table_name}");
 echo "<p>Total $total_num_rows log rows in db</p>";
 
 // Output all loggers
-echo "<b>Loggers</b>";
+echo "<b>Loggers</b><ul>";
 foreach ( $this->instantiatedLoggers as $oneLogger ) {
+
 	// sf_d($oneLogger["name"]);
-	echo "<li>" . $oneLogger["instance"]->slug . "</li>";
+	$logger_info = $oneLogger["instance"]->getInfo();
+	echo "<li>" . $logger_info["name"];
+	echo "<br>" . $logger_info["description"];
+	echo "</li>";
+
 }
+echo "</ul>";
 
 // Output users
-echo "<b>Users</b>";
+echo "<b>Users</b><ul>";
 $sql_users = '
 	SELECT DISTINCT VALUE, wp_users.* FROM wp_simple_history_contexts
 	LEFT JOIN wp_users ON wp_users.id = wp_simple_history_contexts.value
@@ -36,6 +42,7 @@ $user_results = $wpdb->get_results($sql_users);
 foreach ($user_results as $one_user_result) {
 	printf('<li>%3$s</li>', $one_user_result->ID, $one_user_result->user_login, $one_user_result->user_email);
 }
+echo "</ul>";
 
 echo "</div>";
 
