7cde2ad16c3328cbf4c51990927b1bd555de0340
Pär Thernström <par.thernstrom@gmail.com>
2014-07-20 17:20:37 +0200

    log bulk plugin updates

--- loggers/SimplePluginLogger.php
+++ loggers/SimplePluginLogger.php
@@ -47,31 +47,37 @@ class SimplePluginLogger extends SimpleLogger
 			"description" => "Logs plugin installs, uninstalls and updates",
 			"capability" => "activate_plugins", // install_plugins, activate_plugins, 
 			"messages" => array(
+
 				'plugin_activated' => _x(
 					'Activated plugin "{plugin_name}"', 
 					'Plugin was non-silently activated by a user',
 					'simple-history'
 				),
+
 				'plugin_deactivated' => _x(
 					'Deactivated plugin "{plugin_name}"', 
 					'Plugin was non-silently deactivated by a user',
 					'simple-history'
 				),
+
 				'plugin_installed' => _x(
 					'Installed plugin "{plugin_name}"', 
 					'Plugin was installed',
 					'simple-history'
 				),
+
 				'plugin_installed_failed' => _x(
 					'Failed to install plugin "{plugin_name}"', 
 					'Plugin failed to install',
 					'simple-history'
 				),
+
 				'plugin_updated' => _x(
 					'Updated plugin "{plugin_name}"', 
 					'Plugin was updated',
 					'simple-history'
 				),
+
 				'plugin_update_failed' => _x(
 					'Updated plugin "{plugin_name}"', 
 					'Plugin update failed',
@@ -83,6 +89,14 @@ class SimplePluginLogger extends SimpleLogger
 					'Plugin file edited',
 					'simple-history'
 				),
+
+				// bulk versions
+				'plugin_bulk_updated' => _x(
+					'Updated plugin "{plugin_name}"', 
+					'Plugin was updated in bulk',
+					'simple-history'
+				),
+
 
 			)
 		);
@@ -186,7 +200,8 @@ class SimplePluginLogger extends SimpleLogger
 	}
 
 	/**
-	 * Called when WordPress Core is updated
+	 * Called when a single plugin is updated or installed
+	 * (not bulk)
 	 */
 	function on_upgrader_process_complete( $plugin_upgrader_instance, $arr_data ) {
 
@@ -239,7 +254,7 @@ class SimplePluginLogger extends SimpleLogger
 		if ( isset( $arr_data["type"] ) && "plugin" == $arr_data["type"] ) {
 
 			// Single plugin install
-			if ( isset( $arr_data["action"] ) && "install" == $arr_data["action"] ) {
+			if ( isset( $arr_data["action"] ) && "install" == $arr_data["action"] && ! $plugin_upgrader_instance->bulk ) {
 
 				$context = array(
 					"plugin_name" => $plugin_upgrader_instance->skin->api->name,
@@ -275,7 +290,7 @@ class SimplePluginLogger extends SimpleLogger
 
 				}
 
-			}
+			} // install single
 
 			// Single plugin update
 			if ( isset( $arr_data["action"] ) && "update" == $arr_data["action"] ) {
@@ -316,27 +331,73 @@ class SimplePluginLogger extends SimpleLogger
 						$context
 					);
 
+					echo "on_upgrader_process_complete";
+					sf_d( $plugin_upgrader_instance, '$plugin_upgrader_instance' );
+					sf_d( $arr_data, '$arr_data' );
+
 					$did_log = true;
 
 				}
 
-			}
+			} // update single
 		
+
+			/**
+			 * For bulk updates $arr_data looks like:
+			 * Array
+			 * (
+			 *     [action] => update
+			 *     [type] => plugin
+			 *     [bulk] => 1
+			 *     [plugins] => Array
+			 *         (
+			 *             [0] => plugin-folder-1/plugin-index.php
+			 *             [1] => my-plugin-folder/my-plugin.php
+			 *         )
+			 * )
+			 */
+			if ( isset( $arr_data["bulk"] ) && $arr_data["bulk"] && isset( $arr_data["action"] ) && "update" == $arr_data["action"] ) {
+
+				$plugins_updated = isset( $arr_data["plugins"] ) ? (array) $arr_data["plugins"] : array();
+
+				foreach ($plugins_updated as $plugin_name) {
+
+					$plugin_data = get_plugin_data( WP_PLUGIN_DIR . '/' . $plugin_name );
+			
+					$context = array(
+						"plugin_name" => $plugin_data["Name"],
+						"plugin_title" => $plugin_data["Title"],
+						"plugin_description" => $plugin_data["Description"],
+						"plugin_author" => $plugin_data["Author"],
+						"plugin_version" => $plugin_data["Version"],
+						"plugin_url" => $plugin_data["PluginURI"],
+					);
+
+					$this->infoMessage(
+						'plugin_bulk_updated',
+						$context
+					);
+
+				}
+
+			} // bulk update
+
 		
-		}
+		} // if plugin
 
 		if ( ! $did_log ) {
 			echo "on_upgrader_process_complete";
 			sf_d( $plugin_upgrader_instance, '$plugin_upgrader_instance' );
 			sf_d( $arr_data, '$arr_data' );
-			exit;
+			#exit;
 		}
 
 	}
 
 	/*
 	 * Called from filter 'upgrader_post_install'. 
-	 * Used to log plugin installs and updates
+	 *
+	 * Used to log bulk plugin installs and updates
 	 *
 	 * Filter docs:
 	 *
@@ -373,22 +434,42 @@ class SimplePluginLogger extends SimpleLogger
 		if ( isset( $hook_extra["action"] ) && $hook_extra["action"] == "install" && isset( $hook_extra["type"] ) && $hook_extra["type"] == "plugin" ) {
 
 			// It's a plugin install
+			#error_log("plugin install");
 			
 
 		} else if ( isset( $hook_extra["action"] ) && $hook_extra["action"] == "update" && isset( $hook_extra["type"] ) && $hook_extra["type"] == "plugin" ) {
 			
 			// It's a plugin upgrade
-			echo "plugin update!";
+			#echo "plugin update!";
+			error_log("plugin update");
+
+		} else {
+
+			error_log("other");
 
 		}
 
-		#sf_d($response, '$response');
-		#sf_d($hook_extra, '$hook_extra');
-		#sf_d($result, '$result');
+		sf_d($response, '$response');
+		sf_d($hook_extra, '$hook_extra');
+		sf_d($result, '$result');
+		#exit;
 
 		return $response;
 
 	}
+
+	/*
+
+		 * Filter the list of action links available following bulk plugin updates.
+		 *
+		 * @since 3.0.0
+		 *
+		 * @param array $update_actions Array of plugin action links.
+		 * @param array $plugin_info    Array of information for the last-updated plugin.
+
+		$update_actions = apply_filters( 'update_bulk_plugins_complete_actions', $update_actions, $this->plugin_info );
+
+	*/
 
 	/*
 
