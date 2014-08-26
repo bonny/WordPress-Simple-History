88650bcc5445751340fbbf1516e472e7134eabb5
Pär Thernström <par.thernstrom@gmail.com>
2014-08-26 20:25:29 +0200

    better options output and saving permalinks too yo

--- loggers/SimpleOptionsLogger.php
+++ loggers/SimpleOptionsLogger.php
@@ -30,89 +30,89 @@ options white list.
 $whitelist_options = apply_filters( 'whitelist_options', $whitelist_options );
 Array
 (
-    [general] => Array
-        (
-            [0] => blogname
-            [1] => blogdescription
-            [2] => gmt_offset
-            [3] => date_format
-            [4] => time_format
-            [5] => start_of_week
-            [6] => timezone_string
-            [7] => siteurl
-            [8] => home
-            [9] => admin_email
-            [10] => users_can_register
-            [11] => default_role
-        )
+	[general] => Array
+		(
+			[0] => blogname
+			[1] => blogdescription
+			[2] => gmt_offset
+			[3] => date_format
+			[4] => time_format
+			[5] => start_of_week
+			[6] => timezone_string
+			[7] => siteurl
+			[8] => home
+			[9] => admin_email
+			[10] => users_can_register
+			[11] => default_role
+		)
 
-    [discussion] => Array
-        (
-            [0] => default_pingback_flag
-            [1] => default_ping_status
-            [2] => default_comment_status
-            [3] => comments_notify
-            [4] => moderation_notify
-            [5] => comment_moderation
-            [6] => require_name_email
-            [7] => comment_whitelist
-            [8] => comment_max_links
-            [9] => moderation_keys
-            [10] => blacklist_keys
-            [11] => show_avatars
-            [12] => avatar_rating
-            [13] => avatar_default
-            [14] => close_comments_for_old_posts
-            [15] => close_comments_days_old
-            [16] => thread_comments
-            [17] => thread_comments_depth
-            [18] => page_comments
-            [19] => comments_per_page
-            [20] => default_comments_page
-            [21] => comment_order
-            [22] => comment_registration
-        )
+	[discussion] => Array
+		(
+			[0] => default_pingback_flag
+			[1] => default_ping_status
+			[2] => default_comment_status
+			[3] => comments_notify
+			[4] => moderation_notify
+			[5] => comment_moderation
+			[6] => require_name_email
+			[7] => comment_whitelist
+			[8] => comment_max_links
+			[9] => moderation_keys
+			[10] => blacklist_keys
+			[11] => show_avatars
+			[12] => avatar_rating
+			[13] => avatar_default
+			[14] => close_comments_for_old_posts
+			[15] => close_comments_days_old
+			[16] => thread_comments
+			[17] => thread_comments_depth
+			[18] => page_comments
+			[19] => comments_per_page
+			[20] => default_comments_page
+			[21] => comment_order
+			[22] => comment_registration
+		)
 
-    [media] => Array
-        (
-            [0] => thumbnail_size_w
-            [1] => thumbnail_size_h
-            [2] => thumbnail_crop
-            [3] => medium_size_w
-            [4] => medium_size_h
-            [5] => large_size_w
-            [6] => large_size_h
-            [7] => image_default_size
-            [8] => image_default_align
-            [9] => image_default_link_type
-            [10] => uploads_use_yearmonth_folders
-        )
+	[media] => Array
+		(
+			[0] => thumbnail_size_w
+			[1] => thumbnail_size_h
+			[2] => thumbnail_crop
+			[3] => medium_size_w
+			[4] => medium_size_h
+			[5] => large_size_w
+			[6] => large_size_h
+			[7] => image_default_size
+			[8] => image_default_align
+			[9] => image_default_link_type
+			[10] => uploads_use_yearmonth_folders
+		)
 
-    [reading] => Array
-        (
-            [0] => posts_per_page
-            [1] => posts_per_rss
-            [2] => rss_use_excerpt
-            [3] => show_on_front
-            [4] => page_on_front
-            [5] => page_for_posts
-            [6] => blog_public
-        )
+	[reading] => Array
+		(
+			[0] => posts_per_page
+			[1] => posts_per_rss
+			[2] => rss_use_excerpt
+			[3] => show_on_front
+			[4] => page_on_front
+			[5] => page_for_posts
+			[6] => blog_public
+		)
 
-    [writing] => Array
-        (
-            [0] => use_smilies
-            [1] => default_category
-            [2] => default_email_category
-            [3] => use_balanceTags
-            [4] => default_link_category
-            [5] => default_post_format
-            [6] => mailserver_url
-            [7] => mailserver_port
-            [8] => mailserver_login
-            [9] => mailserver_pass
-            [10] => ping_sites
-        )
+	[writing] => Array
+		(
+			[0] => use_smilies
+			[1] => default_category
+			[2] => default_email_category
+			[3] => use_balanceTags
+			[4] => default_link_category
+			[5] => default_post_format
+			[6] => mailserver_url
+			[7] => mailserver_port
+			[8] => mailserver_login
+			[9] => mailserver_pass
+			[10] => ping_sites
+		)
 
 */
 
@@ -136,14 +136,17 @@ class SimpleOptionsLogger extends SimpleLogger
 			"description" => "Logs updates to WordPress settings",
 			"capability" => "manage_options",
 			"messages" => array(
-				'option_updated' => __('Updated option "{option}" on settings page "{option_page}"', "simple-history"),
-                /*
+				//'option_updated' => __('Updated option "{option}" on settings page "{option_page}"', "simple-history"),
+				'option_updated' => __('Updated option "{option}"', "simple-history"),
+				/*
 
-                Updated option "default_comment_status" on settings page "discussion"
-                Edited option "default_comment_status" on settings page "discussion"
-                Modified option "default_comment_status" on settings page "discussion"
-    
-                */
+				Updated option "default_comment_status" on settings page "discussion"
+				Edited option "default_comment_status" on settings page "discussion"
+				Modified option "default_comment_status" on settings page "discussion"
+
+				Edited settings page "discussion" and the "default_comment_status" options
+	
+				*/
 			)
 		);
 		
@@ -154,7 +157,7 @@ class SimpleOptionsLogger extends SimpleLogger
 	function loaded() {
 
 		add_action( 'updated_option', array($this, "on_updated_option"), 10, 3 );
-
+ 
 	}
 
 	function on_updated_option($option, $old_value, $new_value) {
@@ -162,20 +165,25 @@ class SimpleOptionsLogger extends SimpleLogger
 		if (empty( $_SERVER["REQUEST_URI"] )) {
 			return;
 		}
-		
-		// We only want to log options being added via /wp-admin/options.php
-		if ( basename( $_SERVER["REQUEST_URI"] ) !== "options.php" || basename( dirname( $_SERVER["REQUEST_URI"] ) ) !== "wp-admin" ) {
+
+		$arr_option_pages = array(
+			0 => "options.php",
+			1 => "options-permalink.php"
+		);
+
+		// We only want to log options being added via pgaes in $arr_option_pages
+		if ( ! in_array( basename( $_SERVER["REQUEST_URI"] ), $arr_option_pages ) || basename( dirname( $_SERVER["REQUEST_URI"] ) ) !== "wp-admin" ) {
 			return;
 		}
-
+		#echo "save";exit;
 		$this->debugMessage( "option_updated", array(
 			"option" => $option,
 			"old_value" => $old_value,
 			"new_value" => $new_value,
 			"REQUEST_URI" => $_SERVER["REQUEST_URI"],
 			"referer" => wp_get_referer(),
-			"option_page" => isset( $_REQUEST["option_page"] ) ? $_REQUEST["option_page"] : "" // general | discussion | 
-			// "r" => print_r($_REQUEST, true),
+			"option_page" => isset( $_REQUEST["option_page"] ) ? $_REQUEST["option_page"] : "", // general | discussion | 
+			"$_REQUEST" => print_r($_REQUEST, true),
 		) );
 
 
@@ -216,6 +224,40 @@ class SimpleOptionsLogger extends SimpleLogger
 				__("Old value", "simple-history"),
 				esc_html( $context["old_value"] )
 			);
+
+			// If key option_page this was saved from regular settings pages
+			if ( ! empty( $context["option_page"] ) ) {
+
+				$output .= sprintf(
+					'
+					<tr>
+						<td>%1$s</td>
+						<td><a href="%3$s">%2$s</a></td>
+					</tr>
+					',
+					__("Settings page", "simple-history"),
+					esc_html( $context["option_page"] ),
+					admin_url("options-{$context["option_page"]}.php")
+				);
+
+			}
+
+			// If option = permalink_structure then we did it from permalink page
+			if ( ! empty( $context["option"] ) && "permalink_structure" == $context["option"] ) {
+
+				$output .= sprintf(
+					'
+					<tr>
+						<td>%1$s</td>
+						<td><a href="%3$s">%2$s</a></td>
+					</tr>
+					',
+					__("Settings page", "simple-history"),
+					"permalink",
+					admin_url("options-permalink.php")
+				);
+
+			}
 			
 			$output .= "</table>";
 
