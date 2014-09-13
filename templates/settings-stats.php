dc47da68b6e94b6048fe342552c410bcb25d054e
Pär Thernström <par.thernstrom@gmail.com>
2014-09-13 23:51:01 +0200

    add number of logged rows for loggers and users

--- templates/settings-stats.php
+++ templates/settings-stats.php
@@ -67,28 +67,42 @@ echo "<tr>
 		<th>Name</th>
 		<th>Description</th>
 		<th>Capability</th>
+		<th>Rows</th>
 	</tr>";
 foreach ( $this->instantiatedLoggers as $oneLogger ) {
 
-	// sf_d($oneLogger["name"]);
 	$logger_info = $oneLogger["instance"]->getInfo();
+
+	// get number of rows this logger is responsible for
+	$sql_logger_count = sprintf('
+		SELECT count(id) as count
+		FROM %1$s
+		WHERE logger = "%2$s"
+	', $table_name, $oneLogger["instance"]->slug);
+
+	$logger_rows_count = $wpdb->get_var( $sql_logger_count );
+
 	printf(
 		'
 		<tr>
 			<td>%1$s</td>
 			<td>%2$s</td>
 			<td>%3$s</td>
+			<td>%4$s</td>
 		</tr>
 		',
 		esc_html( $logger_info["name"]),
 		esc_html( $logger_info["description"]),
-		esc_html( $logger_info["capability"])
+		esc_html( $logger_info["capability"]),
+		$logger_rows_count
 
 	);
 
 }
 echo "</table>";
 
+// @TODO: stats for logged events by message type, by key perhaps
+// 
 
 // Output users
 echo "<h3>Users that have logged things</h3>";
@@ -97,7 +111,7 @@ echo "<p>Deleted users are also included.";
 
 $sql_users = '
 	SELECT 
-		DISTINCT value, 
+		DISTINCT value as user_id, 
 		wp_users.* 
 	FROM wp_simple_history_contexts
 	LEFT JOIN wp_users ON wp_users.id = wp_simple_history_contexts.value
@@ -114,25 +128,47 @@ echo "<tr>
 		<th>ID</th>
 		<th>login</th>
 		<th>email</th>
+		<th>logged items</th>
 		<th>deleted</th>
 	</tr>";
 
 foreach ($user_results as $one_user_result) {
 	
+	$user_id = $one_user_result->user_id;
+	if ( empty( $user_id ) ) {
+		continue;
+	}
+
 	$str_deleted = empty($one_user_result->user_login) ? "yes" : "";
+
+	// get number of rows this user is responsible for
+	if ($user_id) {
+
+		$sql_user_count = sprintf('
+			SELECT count(value) as count
+			FROM wp_simple_history_contexts
+			WHERE `KEY` = "_user_id"
+			AND value = %1$s
+		', $user_id);
+
+		$user_rows_count = $wpdb->get_var( $sql_user_count );
+
+	}
 
 	printf('
 		<tr>
 			<td>%1$s</td>
 			<td>%2$s</td>
 			<td>%3$s</td>
+			<td>%5$s</td>
 			<td>%4$s</td>
 		</tr>
 		', 
-		$one_user_result->value, 
+		$user_id, 
 		$one_user_result->user_login, 
 		$one_user_result->user_email,
-		$str_deleted
+		$str_deleted,
+		$user_rows_count
 	);
 
 }
