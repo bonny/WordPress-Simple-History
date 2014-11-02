65c5cc03453dbc617afdff594337e1db2af287e3
Pär Thernström <par.thernstrom@gmail.com>
2014-11-02 19:06:00 +0100

    make sure simple_history_add() will continue to work

--- old-functions.php
+++ old-functions.php
@@ -19,44 +19,11 @@ function simple_history_add($args) {
 		"description" => null
 	);
 
-	$args = wp_parse_args( $args, $defaults );
+	$context = wp_parse_args( $args, $defaults );
 
-	$action = esc_sql($args["action"]);
-	$object_type = esc_sql($args["object_type"]);
-	$object_subtype = esc_sql($args["object_subtype"]);
-	$object_id = esc_sql($args["object_id"]);
-	$object_name = esc_sql($args["object_name"]);
-	$user_id = $args["user_id"];
-	$description = esc_sql($args["description"]);
+	$message = "{$context["object_type"]} {$context["object_name"]} {$context["action"]}";
 
-	global $wpdb;
-	$tableprefix = $wpdb->prefix;
-	if ($user_id) {
-		$current_user_id = $user_id;
-	} else {
-		$current_user = wp_get_current_user();
-		$current_user_id = (int) $current_user->ID;
-	}
-
-	// date, store at utc or local time
-	// anything is better than now() anyway!
-	// WP seems to use the local time, so I will go with that too I think
-	// GMT/UTC-time is: date_i18n($timezone_format, false, 'gmt')); 
-	// local time is: date_i18n($timezone_format));
-	$localtime = current_time("mysql");
-	$sql = "
-		INSERT INTO {$tableprefix}simple_history 
-		SET 
-			date = '$localtime', 
-			action = '$action', 
-			object_type = '$object_type', 
-			object_subtype = '$object_subtype', 
-			user_id = '$current_user_id', 
-			object_id = '$object_id', 
-			object_name = '$object_name',
-			action_description = '$description'
-		";
-	$wpdb->query($sql);
+	SimpleLogger()->info($message, $context);
 
 } // simple_history_add
 
