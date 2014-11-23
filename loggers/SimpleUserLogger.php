f0566e682aa4a4a88693c47b4573981e6ea6dc03
Pär Thernström <par.thernstrom@gmail.com>
2014-11-23 13:15:53 +0100

    add filter to log password for non existing users too

--- loggers/SimpleUserLogger.php
+++ loggers/SimpleUserLogger.php
@@ -334,6 +334,20 @@ class SimpleUserLogger extends SimpleLogger
 				"_occasionsID" => __CLASSNAME__  . '/' . __FUNCTION__
 			);
 
+			/**
+			 * Maybe store password too
+			 * Default is to not do this because of privacy and security
+			 *
+			 * @since 2.0
+			 *
+			 * @param bool $log_password
+			 */
+			$log_password = false;
+			$log_password = apply_filters("simple_history/comments_logger/log_not_existing_user_password", $log_password);
+			if ($log_password) {
+				$context["login_user_password"] = $password;
+			}
+
 			$this->warningMessage("user_unknown_login_failed", $context);		
 
 		}
