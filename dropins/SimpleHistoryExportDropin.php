de724b1c6396631bccbf4b3f3ea4ccbdd10f8c0e
Pär Thernström <par.thernstrom@gmail.com>
2014-12-26 00:14:31 +0100

    add dropin file with tab

---
+++ dropins/SimpleHistoryExportDropin.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+Dropin Name: Export
+Dropin Description: Adds a tab with export options
+Dropin URI: http://simple-history.com/
+Author: Pär Thernström
+*/
+
+class SimpleHistoryExportDropin {
+
+	// Simple History instance
+	private $sh;
+
+	public function __construct($sh) {
+
+		// Since it's not quite done yet, it's for da devs only for now
+		if ( ! defined("SIMPLE_HISTORY_DEV") || ! SIMPLE_HISTORY_DEV ) {
+			return;
+		}
+
+		// Set simple history variable
+		$this->sh = $sh;
+
+		// Add tab to settings page
+		$sh->registerSettingsTab(array(
+			"slug" => "export",
+			"name" => _x("Export", "Tab name on settings page", "simple-history"),
+			"function" => array($this, "output")
+		));
+
+	}
+
+
+	public function output() {
+
+		global $wpdb;
+		$table_name = $wpdb->prefix . SimpleHistory::DBTABLE;
+		$table_name_contexts = $wpdb->prefix . SimpleHistory::DBTABLE_CONTEXTS;
+
+		?>
+		<!-- <h2>Export</h2> -->
+
+		<p>The export function will export the full history.</p>
+
+		<h3>Format</h3>
+	
+		<p>
+			<label>
+				<input type="radio" name="format" value="csv" checked>
+				CSV
+			</label>
+			<br>
+
+			<label>
+				<input type="radio" name="format" value="json">
+				JSON
+			</label>
+			<br>
+
+			<label>
+				<input type="radio" name="format" value="html">
+				HTML
+			</label>
+		</p>
+
+		<p>
+			<button type="submit" class="button button-primary">Export</button>
+		</p>
+		<?php
+
+	}
+
+}
+
