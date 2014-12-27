c8d336515272a5938c8ac1bdff2fec4bf8485892
Pär Thernström <par.thernstrom@gmail.com>
2014-12-28 00:43:22 +0100

    add popup/tooltip for ip adress using ipinfo.io

--- dropins/SimpleHistoryIpInfoDropin.js
+++ dropins/SimpleHistoryIpInfoDropin.js
@@ -2,7 +2,13 @@
 (function($) {
 
 	var $logItems = $(".SimpleHistoryLogitems");
+	var $popup = $(".SimpleHistoryIpInfoDropin__popup");
+	var $popupContent = $popup.find(".SimpleHistoryIpInfoDropin__popupContent");
 
+	var templateLoading = _.template( $("#tmpl-simple-history-ipinfodropin-popup-loading").text() );
+	var templateLoaded = _.template( $("#tmpl-simple-history-ipinfodropin-popup-loaded").text() );
+
+	// Click on link with IP-number
 	$logItems.on("click", ".SimpleHistoryLogitem__anonUserWithIp__theIp", function(e) {
 
 		var $elm = $(this);
@@ -12,10 +18,31 @@
 			return;
 		}
 
+		showPopup($elm);
+
 		return lookupIpAddress(ipAddress);
 
 	});
 
+	// Position and then show popup.
+	// Content is not added yet
+	function showPopup($elm) {
+
+		var offset = $elm.offset();
+
+		$popup.css({
+			//top: offset.top + $elm.outerHeight(),
+			top: offset.top,
+			left: offset.left
+		});
+
+		$popupContent.html(templateLoading());
+
+		$popup.addClass("is-visible");
+
+	}
+
+	// Init request to lookup address
 	function lookupIpAddress(ipAddress) {
 
 		$.get("http://ipinfo.io/" + ipAddress, onIpAddressLookupkResponse, "jsonp");
@@ -24,9 +51,12 @@
 
 	}
 
+	// Functin called when ip adress lookup succeeded
 	function onIpAddressLookupkResponse(d) {
 
 		console.log("got data", d);
+		
+		$popupContent.html(templateLoaded(d));
 
 	}
 
--- dropins/SimpleHistoryIpInfoDropin.php
+++ dropins/SimpleHistoryIpInfoDropin.php
@@ -20,6 +20,8 @@ class SimpleHistoryIpInfoDropin {
 		}
 
 		add_action("simple_history/enqueue_admin_scripts", array($this, "enqueue_admin_scripts"));
+		add_action("simple_history/admin_footer", array($this, "add_js_template"));
+
 
 	}
 
@@ -31,6 +33,147 @@ class SimpleHistoryIpInfoDropin {
 
 		wp_enqueue_style("simple_history_IpInfoDropin", $file_url . "SimpleHistoryIpInfoDropin.css", null, SimpleHistory::VERSION);
 
+	}
+
+	public function add_js_template() {
+		?>
+
+		<div class="SimpleHistoryIpInfoDropin__popup">
+			<div class="xdashicons xdashicons-arrow-up SimpleHistoryIpInfoDropin__popupArrow"></div>
+			<div class="SimpleHistoryIpInfoDropin__popupContent"></div>
+		</div>
+
+		<script type="text/html" id="tmpl-simple-history-ipinfodropin-popup-loading">
+			<p>Getting IP info ...</p>
+		</script>
+
+		<script type="text/html" id="tmpl-simple-history-ipinfodropin-popup-loaded">
+			<!--
+			{
+			  "ip": "8.8.8.8",
+			  "hostname": "google-public-dns-a.google.com",
+			  "city": "Mountain View",
+			  "region": "California",
+			  "country": "US",
+			  "loc": "37.3860,-122.0838",
+			  "org": "AS15169 Google Inc.",
+			  "postal": "94035"
+			}
+			-->
+			<% if ( typeof(bogon) != "undefined" ) { %>
+
+				<p>Does not seem like a public IP address</p>
+				
+			<% } else { %>
+				
+				<table class="SimpleHistoryIpInfoDropin__ipInfoTable">
+
+					<tr class="SimpleHistoryIpInfoDropin__ipInfoTable__mapRow">
+						<td colspan="2">
+							<% if ( typeof(loc) != "undefined" ) { %>
+								<img src="https://maps.googleapis.com/maps/api/staticmap?center=<%= loc %>&zoom=7&size=300x100&sensor=false" alt="Google Map">
+							<% } %>
+						</td>
+					</tr>
+
+					<% if ( typeof(ip) != "undefined" ) { %>
+					<tr>
+						<td>
+							IP address
+						</td>
+						<td>
+							<%= ip %>
+						</td>
+					</tr>
+					<% } %>
+
+					<% if ( typeof(hostname) != "undefined" ) { %>
+					<tr>
+						<td>
+							Hostname
+						</td>
+						<td>
+							<%= hostname %>
+						</td>
+					</tr>
+					<% } %>
+
+					<% if ( typeof(org) != "undefined" ) { %>
+					<tr>
+						<td>
+							Network
+						</td>
+						<td>
+							<%= org %>
+						</td>
+					</tr>
+					<% } %>
+
+					<% if ( typeof(network) != "undefined" ) { %>
+					<tr>
+						<td>
+							Network
+						</td>
+						<td>
+							<%= network %>
+						</td>
+					</tr>
+					<% } %>
+
+					<% if ( typeof(city) != "undefined" ) { %>
+					<tr>
+						<td>
+							City
+						</td>
+						<td>
+							<%= city %>
+						</td>
+					</tr>
+					<% } %>			
+
+					<% if ( typeof(region) != "undefined" ) { %>
+					<tr>
+						<td>
+							Region
+						</td>
+						<td>
+							<%= region %>
+						</td>
+					</tr>
+					<% } %>
+
+					<% if ( typeof(country) != "undefined" ) { %>
+					<tr>
+						<td>
+							Country
+						</td>
+						<td>
+							<%= country %>
+						</td>
+					</tr>
+					<% } %>
+
+					<!--
+					<% if ( typeof(loc) != "undefined" ) { %>
+					<tr>
+						<td>
+							loc
+						</td>
+						<td>
+							<%= loc %>
+						</td>
+					</tr>
+					<% } %>
+					-->
+
+				</table>
+
+				<p class="SimpleHistoryIpInfoDropin__provider">IP info provided by <a href="http://ipinfo.io/">ipinfo.io</a>.</p>
+
+			<% } %>
+
+		</script>
+		<?php
 	}
 
 } // end class
