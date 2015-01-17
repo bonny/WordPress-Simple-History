4a79f475f4a1933b457deba9cb1cf1f22295c0ba
Pär Thernström <par.thernstrom@gmail.com>
2015-01-17 15:05:31 +0100

    update sidebar texts + make them translatable

--- dropins/SimpleHistorySidebarDropin.php
+++ dropins/SimpleHistorySidebarDropin.php
@@ -4,7 +4,7 @@ Dropin Name: Sidebar
 Drop Description: Outputs HTML and filters for a sidebar
 Dropin URI: http://simple-history.com/
 Author: Pär Thernström
-*/
+ */
 
 class SimpleHistorySidebarDropin {
 
@@ -15,10 +15,10 @@ class SimpleHistorySidebarDropin {
 		$this->sh = $sh;
 
 		add_action("simple_history/enqueue_admin_scripts", array($this, "enqueue_admin_scripts"));
-		add_action("simple_history/history_page/after_gui", array( $this, "output_sidebar_html") );
+		add_action("simple_history/history_page/after_gui", array($this, "output_sidebar_html"));
 
 		// add_action("simple_history/dropin/sidebar/sidebar_html", array($this, "example_output"));
-		
+
 		add_action("simple_history/dropin/sidebar/sidebar_html", array($this, "default_sidebar_contents"));
 
 	}
@@ -27,35 +27,62 @@ class SimpleHistorySidebarDropin {
 
 		// Boxes that will appear randomly
 
+		// Box about GitHub
+		$headline = _x("Simple History is on GitHub", 'Sidebar box', 'simple-history');
+		
+		$body = sprintf(
+				_x('You can star, fork, or report issues with this plugin over at the <a href="%1$s">GitHub page</a>.', 'Sidebar box', 'simple-history'),
+				'https://github.com/bonny/WordPress-Simple-History'
+			);
+
 		$boxGithub = '
 			<div class="postbox">
-				<h3 class="hndle">Simple History is on GitHub</h3>
+				<h3 class="hndle">'.$headline.'</h3>
 				<div class="inside">
-					<p>Star, fork, or report issues with this plugin over
-					at the <a href="https://github.com/bonny/WordPress-Simple-History">Simple History Github page</a></p>
+					<p>'.$body.'</p>
 				</div>
 			</div>
 		';
 
+		// Box about donation
+		$headline = _x('Donate to support development', 'Sidebar box', 'simple-history');
 
+		$body = sprintf(
+				_x('If you like and use Simple History you should <a href="%1$s">donate to keep this plugin free</a>.', 'Sidebar box', 'simple-history'),
+				'http://eskapism.se/sida/donate/'
+			);
+		
 		$boxDonate = '
 			<div class="postbox">
-				<h3 class="hndle">Donate to support development</h3>
+				<h3 class="hndle">'.$headline.'</h3>
 				<div class="inside">
-					<p>If you like and use Simple History consider <a href="eskapism.se/sida/donate/">donating</a>.</p>
+					<p>'.$body.'</p>
 				</div>
 			</div>
 		';
+
+		// Box about review
+		$headline = _x('Review this plugin if you like it', 'Sidebar box', 'simple-history');
+
+		$body1 = sprintf(
+					_x('If you like Simple History then please <a href="%1$s">give it a nice review over at wordpress.org</a>.', 'Sidebar box', 'simple-history'),
+					'https://wordpress.org/support/view/plugin-reviews/simple-history'
+				);
+
+		$body2 = _x('A good review can help new users find this plugin. And it will boost the ego of the author.', 'Sidebar box', 'simple-history');
 
 		$boxReview = '
 			<div class="postbox">
-				<h3 class="hndle">Help others find Simple History by giving it a review</h3>
+				<h3 class="hndle">'.$headline.'</h3>
 				<div class="inside">
-					<p><a href="https://wordpress.org/support/view/plugin-reviews/simple-history">Give it a nice review over at wordpress.org</a>.</p>
+					<p>'.$body1.'</p>
+					<p>'.$body2.'</p>
 				</div>
 			</div>
 		';
 
+		// Box about tweeting and blogging
+		/*
 		$boxSocial = '
 			<div class="postbox">
 				<h3 class="hndle">Blog or tweet</h3>
@@ -64,48 +91,9 @@ class SimpleHistorySidebarDropin {
 				</div>
 			</div>
 		';
+		*/
 
-		$arrBoxes = array($boxGithub, $boxDonate, $boxReview, $boxSocial);
-
-		//echo $arrBoxes[array_rand($arrBoxes)];
-		echo implode("", $arrBoxes); // show all
-
-		// Box to encourage people translate plugin
-		$current_locale = get_locale();
-		if ( "en_US" != $current_locale ) {
-
-			/** WordPress Translation Install API */
-			require_once( ABSPATH . 'wp-admin/includes/translation-install.php' );
-			$translations = wp_get_available_translations();
-
-			// This text is only shown in English, so no need for translation of it
-			$boxTranslationTmpl = '
-				<div class="postbox">
-					<h3 class="hndle">Translate Simple History to %1$s</h3>
-					<div class="inside">
-						<p>It looks like Simple History is not yet translated to your language.</p>
-
-						<p>If you\'re interested in translating it please check out the <a href="https://developer.wordpress.org/plugins/internationalization/localization/">localization</a> part of the Plugin Handbook for info on how to translate plugins.
-						</p>
-
-						<p>When you\'re done with your translation email it to me at <a href="mailto:par.thernstrom@gmail.com" rel="nofollow">par.thernstrom@gmail.com</a>, or <a href="https://github.com/bonny/WordPress-Simple-History/" rel="nofollow">add a pull request</a>.</p>
-					</div>
-				</div>
-			';
-
-			if ( isset( $translations[$current_locale] ) ) {
-				
-				// Check if an existing text string returns something else, and that current lang is not en
-				$teststring_translated = __("Just now", "simple-history");
-				$teststring_untranslated = "Just now";
-				if ( $teststring_untranslated == $teststring_translated ) {
-					// strings are the same, so plugin probably not translated
-					printf($boxTranslationTmpl, $translations[$current_locale]["english_name"]);
-				}
-				
-			}
-		}
-
+		// Box about possible events missing
 		$boxMissingEvents = '
 			<div class="postbox">
 				<h3 class="hndle">Missing events?</h3>
@@ -116,18 +104,67 @@ class SimpleHistorySidebarDropin {
 		';
 		//echo $boxMissingEvents;
 
+
+		$arrBoxes = array($boxGithub, $boxDonate, $boxReview);
+
+		//echo $arrBoxes[array_rand($arrBoxes)];
+		echo implode("", $arrBoxes); // show all
+
+		// Box to encourage people translate plugin
+		$current_locale = get_locale();
+		if ("en_US" != $current_locale) {
+
+			/** WordPress Translation Install API */
+			require_once ABSPATH . 'wp-admin/includes/translation-install.php';
+			
+			$translations = wp_get_available_translations();
+
+			// This text does not need translation since is's only shown in English
+			$boxTranslationTmpl = '
+				<div class="postbox">
+					<h3 class="hndle">Translate Simple History to %1$s</h3>
+					<div class="inside">
+						
+						<p>
+							It looks like Simple History is not yet translated to your language.
+						</p>
+
+						<p>
+							If you\'re interested in translating it please check out the <a href="https://developer.wordpress.org/plugins/internationalization/localization/">localization</a> part of the Plugin Handbook for info on how to translate plugins.
+						</p>
+
+						<p>
+							When you\'re done with your translation email it to me at <a href="mailto:par.thernstrom@gmail.com" rel="nofollow">par.thernstrom@gmail.com</a>, or <a href="https://github.com/bonny/WordPress-Simple-History/" rel="nofollow">add a pull request</a>.
+						</p>
+					</div>
+				</div>
+			';
+
+			if (isset($translations[$current_locale])) {
+
+				// Check if an existing text string returns something else, and that current lang is not en
+				$teststring_translated = __("Just now", "simple-history");
+				$teststring_untranslated = "Just now";
+				if ($teststring_untranslated == $teststring_translated) {
+					// strings are the same, so plugin probably not translated
+					printf($boxTranslationTmpl, $translations[$current_locale]["english_name"]);
+				}
+
+			}
+		}
+
 	}
 
 	public function example_output() {
 		?>
-		<div class="postbox">
+<div class="postbox">
 			<h3 class="hndle">Example title</h3>
 			<div class="inside">
 				<p>Example content. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Inquit, dasne adolescenti veniam? Non laboro, inquit, de nomine. In quibus doctissimi illi veteres inesse quiddam caeleste et divinum putaverunt. Duo Reges: constructio interrete. Indicant pueri, in quibus ut in speculis natura cernitur. Quod ea non occurrentia fingunt, vincunt Aristonem; Quod quidem iam fit etiam in Academia. Aliter enim nosmet ipsos nosse non possumus.</p>
 			</div>
 		</div>
-		<?php
-	}
+<?php
+}
 
 	public function enqueue_admin_scripts() {
 
@@ -155,24 +192,23 @@ class SimpleHistorySidebarDropin {
 	public function output_sidebar_html() {
 
 		?>
-		<div class="SimpleHistory__pageSidebar">
+<div class="SimpleHistory__pageSidebar">
 
 			<div class="metabox-holder">
 
-				<?php
-				/**
-				 * Allows to output HTML in sidebar
-				 *
-				 * @since 2.0.16
-				 */
-				do_action("simple_history/dropin/sidebar/sidebar_html");
-				?>
-
-			</div>
+<?php
+/**
+		 * Allows to output HTML in sidebar
+		 *
+		 * @since 2.0.16
+		 */
+		do_action("simple_history/dropin/sidebar/sidebar_html");
+		?>
+</div>
 
 		</div>
-		<?php
+<?php
 
 	}
 
-} // end class
+}// end class
