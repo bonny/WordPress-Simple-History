7bf04df9851314209728c9fc8f0372f40ec15a87
Pär Thernström <par.thernstrom@gmail.com>
2015-01-20 11:04:51 +0100

    json encode debug vals

--- loggers/SimpleUserLogger.php
+++ loggers/SimpleUserLogger.php
@@ -303,11 +303,11 @@ class SimpleUserLogger extends SimpleLogger
 
 			// Some temp debug things
 			$context["_debug_user_login"] = $user_login;
-			$context["_debug_user"] = $user;
-			$context["_debug_server"] = $_SERVER;
-			$context["_debug_get"] = $_GET;
-			$context["_debug_get"] = $_POST;
-			$context["_debug_cookies"] = $_COOKIES;
+			$context["_debug_user"] = simpleHistory::json_encode($user);
+			$context["_debug_server"] = simpleHistory::json_encode($_SERVER);
+			$context["_debug_get"] = simpleHistory::json_encode($_GET);
+			$context["_debug_get"] = simpleHistory::json_encode($_POST);
+			$context["_debug_cookies"] = simpleHistory::json_encode($_COOKIES);
 
 			$this->warningMessage("user_unknown_logged_in", $context );
 
