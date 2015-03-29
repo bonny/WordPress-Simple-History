d8da7cfac37e7800450472761d065bdd564486a7
Pär Thernström <par.thernstrom@gmail.com>
2015-03-29 13:48:48 +0200

    fix possible notice warning in comments logger
    Got "Undefined index: pingback_author_email” on one site :/

--- loggers/SimpleCommentsLogger.php
+++ loggers/SimpleCommentsLogger.php
@@ -716,12 +716,16 @@ class SimpleCommentsLogger extends SimpleLogger
 					break;
 
 				default;
-					$desc_output = esc_html( $context[ $key ] );
+					
+					if ( isset( $context[ $key ] ) ) {
+						$desc_output = esc_html( $context[ $key ] );
+					}
+
 					break;
 			}
 
 			// Skip empty rows
-			if (empty( $desc_output )) {
+			if ( empty( $desc_output ) ) {
 				continue;
 			}
 
