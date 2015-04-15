5b34e63dc0a79856836d677bf645c8ff2491ad17
Pär Thernström <par.thernstrom@gmail.com>
2015-04-15 21:19:26 +0200

    add simple_history_text_diff() for diffing text

--- inc/SimpleHistory.php
+++ inc/SimpleHistory.php
@@ -2670,3 +2670,93 @@ function simple_history_add($args) {
 	SimpleLogger()->info($message, $context);
 
 } // simple_history_add
+
+/**
+ * Pretty much same as wp_text_diff() but with this you can set leading and trailing context lines
+ *
+ * @since 2.0.x
+ *
+ * 
+ * Original description from wp_text_diff():
+ *
+ * Displays a human readable HTML representation of the difference between two strings.
+ *
+ * The Diff is available for getting the changes between versions. The output is
+ * HTML, so the primary use is for displaying the changes. If the two strings
+ * are equivalent, then an empty string will be returned.
+ *
+ * The arguments supported and can be changed are listed below.
+ *
+ * 'title' : Default is an empty string. Titles the diff in a manner compatible
+ *		with the output.
+ * 'title_left' : Default is an empty string. Change the HTML to the left of the
+ *		title.
+ * 'title_right' : Default is an empty string. Change the HTML to the right of
+ *		the title. 
+ *
+ * @see wp_parse_args() Used to change defaults to user defined settings.
+ * @uses Text_Diff
+ * @uses WP_Text_Diff_Renderer_Table
+ *
+ * @param string $left_string "old" (left) version of string
+ * @param string $right_string "new" (right) version of string
+ * @param string|array $args Optional. Change 'title', 'title_left', and 'title_right' defaults. And leading_context_lines and trailing_context_lines.
+ * @return string Empty string if strings are equivalent or HTML with differences.
+ */
+function simple_history_text_diff( $left_string, $right_string, $args = null ) {
+	
+	$defaults = array( 
+		'title' => '', 
+		'title_left' => '', 
+		'title_right' => '',
+		"leading_context_lines" => 1,
+		"trailing_context_lines" => 1
+	);
+
+	$args = wp_parse_args( $args, $defaults );
+
+	if ( !class_exists( 'WP_Text_Diff_Renderer_Table' ) )
+		require( ABSPATH . WPINC . '/wp-diff.php' );
+
+	$left_string  = normalize_whitespace($left_string);
+	$right_string = normalize_whitespace($right_string);
+
+	$left_lines  = explode("\n", $left_string);
+	$right_lines = explode("\n", $right_string);
+	$text_diff = new Text_Diff($left_lines, $right_lines);
+
+	$renderer  = new WP_Text_Diff_Renderer_Table( $args );
+	$renderer->_leading_context_lines = $args["leading_context_lines"];
+	$renderer->_trailing_context_lines = $args["trailing_context_lines"];
+
+	$diff = $renderer->render($text_diff);
+
+	if ( !$diff )
+		return '';
+
+	$r  = "<table class='diff SimpleHistory__diff'>\n";
+
+	if ( ! empty( $args[ 'show_split_view' ] ) ) {
+		$r .= "<col class='content diffsplit left' /><col class='content diffsplit middle' /><col class='content diffsplit right' />";
+	} else {
+		$r .= "<col class='content' />";
+	}
+
+	if ( $args['title'] || $args['title_left'] || $args['title_right'] )
+		$r .= "<thead>";
+	if ( $args['title'] )
+		$r .= "<tr class='diff-title'><th colspan='4'>$args[title]</th></tr>\n";
+	if ( $args['title_left'] || $args['title_right'] ) {
+		$r .= "<tr class='diff-sub-title'>\n";
+		$r .= "\t<td></td><th>$args[title_left]</th>\n";
+		$r .= "\t<td></td><th>$args[title_right]</th>\n";
+		$r .= "</tr>\n";
+	}
+	if ( $args['title'] || $args['title_left'] || $args['title_right'] )
+		$r .= "</thead>\n";
+
+	$r .= "<tbody>\n$diff\n</tbody>\n";
+	$r .= "</table>";
+
+	return $r;
+}
