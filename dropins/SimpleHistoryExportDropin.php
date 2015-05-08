c7af9cdd5ece65656cc2159bd23e1f6b08360160
Pär Thernström <par.thernstrom@gmail.com>
2015-05-08 09:49:25 +0200

    begin json output

--- dropins/SimpleHistoryExportDropin.php
+++ dropins/SimpleHistoryExportDropin.php
@@ -47,43 +47,20 @@ class SimpleHistoryExportDropin {
 
 			$export_format = isset( $_POST["format"] ) ? $_POST["format"] : "json";
 
-			/*
-			// Get all history
-			// @todo: need examples of that. I wrote the api. I dunno how to use it. *facepalm*
-			// get mem problem right away. crap.
-			use unbuffered queries, using MYSQLI_USE_RESULT
-			https://make.wordpress.org/core/2014/04/07/mysql-in-wordpress-3-9/
-			http://stackoverflow.com/questions/12663544/export-large-database-to-file-without-mysqldump-low-memory-footprint
-			http://php.net/manual/en/mysqli.use-result.php
-			http://php.net/manual/en/function.mysql-unbuffered-query.php
-			if ( $wpdb->use_mysqli ) {
-				$mysql_ver = @mysqli_get_server_info( $wpdb->dbh );
-			} else {
-				$mysql_ver = @mysql_get_server_info();
-			}
-
-			*/
-
-			#d($events_sql);exit;
-			#$sql = "SELECT * FROM {$table_name_contexts}";
-
-			/*global $wbdp;
-			if( $wpdb->use_mysqli ) {
-				$rows = mysqli_query($wpdb->dbh, $sql, MYSQLI_USE_RESULT);
-			} else {
-				$rows = mysql_unbuffered_query($sql, $wpdb->dbh);
-			}*/
-
+			// Disable relative time output in header
 			add_filter("simple_history/header_time_ago_max_time", "__return_zero");
 			add_filter("simple_history/header_just_now_max_time", "__return_zero");
+
+			// Don't use "You" if event is initiated by the same user that does the export
 			add_filter("simple_history/header_initiator_use_you", "__return_false");
 
-			// Paginate through all rows
 			$query = new SimpleHistoryLogQuery();
+
 			$query_args = array(
 				"paged" => 1,
 				"posts_per_page" => 5000
 			);
+
 			$events = $query->query($query_args);
 
 			// $events->total_row_count;
@@ -93,68 +70,98 @@ class SimpleHistoryExportDropin {
 			$fp = fopen('php://output', 'w');
 
 			#header("Content-Type: application/octet-stream");
-			header("Content-Type: text/plain");
-			#header("Content-Disposition: attachment; filename='export.txt'");
+			if ( "csv" == $export_format ) {
 
+				$filename = "simple-history-export-" . time() . ".csv";
+				header("Content-Type: text/plain");
+				header("Content-Disposition: attachment; filename='{$filename}'");
+
+			} else if ( "json" == $export_format ) {
+
+				$filename = "simple-history-export-" . time() . ".json";
+				header("Content-Type: application/json");
+				header("Content-Disposition: attachment; filename='{$filename}'");
+
+			}
+
+			if ("json" == $export_format) {
+
+				$json_row = "[";
+				fwrite($fp, $json_row);
+
+			}
+
+			// Paginate through all pages and all their rows
+			$row_loop = 0;
 			while ( $page_current <= $pages_count + 1 ) {
 
-				// echo "<br>exporting page $page_current of $pages_count";
+				// if ($page_current > 1) { break; } # To debug/test
 
-				foreach ($events["log_rows"] as $one_row)  {
+				foreach ( $events["log_rows"] as $one_row )  {
 
-					#$message = sprintf('%1$s: %2$s', $one_row->date, $one_row->message);
+					// if ( $row_loop > 2) { break; } # To debug/test
+
 					$header_output = strip_tags( html_entity_decode( $this->sh->getLogRowHeaderOutput( $one_row ), ENT_QUOTES, 'UTF-8') );
-					#$header_output = str_replace(array("\r", "\n"), '', $header_output);
 					$header_output = trim(preg_replace('/\s\s+/', ' ', $header_output));
 
 					$message_output = strip_tags( html_entity_decode( $this->sh->getLogRowPlainTextOutput( $one_row ), ENT_QUOTES, 'UTF-8') );
-					#$details_output = $this->sh->getLogRowDetailsOutput( $row );
 
-					#$text_output = "{$header_output} – {$message_output}\n";
+					if ("csv" == $export_format) {
 
-					print_r($one_row);exit;
-					fputcsv($fp, array(
-						$one_row->date,
-						$one_row->logger,
-						$one_row->level,
-						$one_row->initiator,
-						$one_row->context_message_key,
-						$header_output,
-						$message_output,
-						$one_row->subsequentOccasions
-					));
+						fputcsv($fp, array(
+							$one_row->date,
+							$one_row->logger,
+							$one_row->level,
+							$one_row->initiator,
+							$one_row->context_message_key,
+							$header_output,
+							$message_output,
+							$one_row->subsequentOccasions
+						));
 
-					#fwrite($fp, $text_output);
-					/*
-					fwrite($fp,"Hello, world !<BR>\n");
-					fclose($fp);
-					*/
-					flush();
-					exit;
+					} else if ("json" == $export_format) {
+
+						// If not first loop then add a comma between all json objects
+						if ( $row_loop == 0 ) {
+							$comma = "\n";
+						} else {
+							$comma = ",\n";
+						}
+
+						$json_row = $comma . $this->sh->json_encode($one_row);
+						fwrite($fp, $json_row);
+
+					}
+
+					$row_loop++;
 
 				}
 
 				#d(memory_get_usage());
 				#d(memory_get_peak_usage());
-				flush();
-
-				#exit;
-				#d($query_args);exit;
+				#flush();
 
 				// Fetch next page
+				// @TODO: must take into consideration that new items can be added while we do the fetch
 				$page_current++;
 				$query_args["paged"] = $page_current;
 				$events = $query->query($query_args);
 
+			}
+
+			if ("json" == $export_format) {
+
+				$json_row = "]";
+				fwrite($fp, $json_row);
 
 			}
 
 			fclose($fp);
+			flush();
 
-			#d($events);
+			exit;
 
-
-			echo "<br>done";
+			#echo "<br>done";
 
 		}
 
