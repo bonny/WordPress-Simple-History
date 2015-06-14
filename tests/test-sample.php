d3e05414aa34b283bb7f69f543499d001886e7d5
Pär Thernström <par.thernstrom@gmail.com>
2015-06-14 11:38:50 +0200

    add some very basic tests

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -3,8 +3,22 @@
 class SampleTest extends WP_UnitTestCase {
 
 	function test_sample() {
+
 		// replace this with some actual testing code
 		$this->assertTrue( true );
 	}
+
+	function test_history_setup() {
+
+		$this->assertTrue( class_exists("SimpleHistory") );
+		$this->assertTrue( class_exists("SimpleHistoryLogQuery") );
+
+		$this->assertTrue( defined("SIMPLE_HISTORY_VERSION") );
+		$this->assertTrue( defined("SIMPLE_HISTORY_PATH") );
+		$this->assertTrue( defined("SIMPLE_HISTORY_BASENAME") );
+		$this->assertTrue( defined("SIMPLE_HISTORY_FILE") );
+
+	}
+
 }
 
