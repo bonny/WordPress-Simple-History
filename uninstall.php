a25673cd78faf54bdfbfc31879d8b9f36454a620
Pär Thernström <par.thernstrom@gmail.com>
2017-10-28 16:55:06 +0200

    phpcbf on uninstall file

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,14 @@
 <?php
 /**
  * File that is run during plugin uninstall (not just de-activate)
+ *
  * @TODO: delete all tables in network if on multisite
  */
 
 // If uninstall not called from WordPress exit
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
-	exit ();
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
 
 /*
 Go on with uninstall actions:
@@ -16,24 +18,24 @@ Go on with uninstall actions:
 
 // Remove options
 $arr_options = array(
- 	"simple_history_pager_size",
- 	"simple_history_db_version",
- 	"simple_history_rss_secret",
- 	"simple_history_show_on_dashboard",
- 	"simple_history_show_as_page"
+	 'simple_history_pager_size',
+	 'simple_history_db_version',
+	 'simple_history_rss_secret',
+	 'simple_history_show_on_dashboard',
+	 'simple_history_show_as_page',
 );
 
-foreach ($arr_options as $one_option) {
+foreach ( $arr_options as $one_option ) {
 	delete_option( $one_option );
 }
 
 global $wpdb;
 
 // Remove database tables
-$table_name = $wpdb->prefix . "simple_history";
-$wpdb->query("DROP TABLE IF EXISTS $table_name");
+$table_name = $wpdb->prefix . 'simple_history';
+$wpdb->query( "DROP TABLE IF EXISTS $table_name" );
 
-$table_name = $wpdb->prefix . "simple_history_contexts";
-$wpdb->query("DROP TABLE IF EXISTS $table_name");
+$table_name = $wpdb->prefix . 'simple_history_contexts';
+$wpdb->query( "DROP TABLE IF EXISTS $table_name" );
 
 // And we are done. Simple History is ...  history.
