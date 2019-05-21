2d28ae3d859d4c8a3de2f570441f05cd097bec29
Pär Thernström <par.thernstrom@gmail.com>
2019-05-21 22:39:39 +0200

    Format code + use correct version in index file

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
  * Plugin URI: http://simple-history.com
  * Text Domain: simple-history
  * Domain Path: /languages
- * Description: Plugin that logs various things that occur in WordPress and then presents those events in a very nice GUI.
- * Version: 2.29.2
+ * Description: Plugin that logs various things that occur in WordPress and then
+ *              presents those events in a very nice GUI.
+ * Version: 2.31.1
  * Author: Pär Thernström
  * Author URI: http://simple-history.com/
  * License: GPL2
@@ -29,7 +30,7 @@
  */
 
 if (!defined('WPINC')) {
-	die();
+    die();
 }
 
 // Plugin requires at least WordPress version "4.5.1", because usage of functions like wp_get_raw_referer.
@@ -38,93 +39,80 @@ $ok_wp_version = version_compare($GLOBALS['wp_version'], '4.5.1', '>=');
 $ok_php_version = version_compare(phpversion(), '5.3', '>=');
 
 if ($ok_php_version && $ok_wp_version) {
-	/**
-	 * Register function that is called when plugin is installed
-	 *
-	 * @TODO: make activatigon multi site aware, as in https://github.com/scribu/wp-proper-network-activation
-	 * register_activation_hook( trailingslashit(WP_PLUGIN_DIR) . trailingslashit( plugin_basename(__DIR__) ) . "index.php" , array("SimpleHistory", "on_plugin_activate" ) );
-	 */
+    /**
+     * Register function that is called when plugin is installed
+     *
+     * @TODO: make activatigon multi site aware, as in https://github.com/scribu/wp-proper-network-activation
+     * register_activation_hook( trailingslashit(WP_PLUGIN_DIR) . trailingslashit( plugin_basename(__DIR__) ) . "index.php" , array("SimpleHistory", "on_plugin_activate" ) );
+     */
 
-	if (!defined('SIMPLE_HISTORY_VERSION')) {
-		define('SIMPLE_HISTORY_VERSION', '2.31.1');
-	}
+    if (!defined('SIMPLE_HISTORY_VERSION')) {
+        define('SIMPLE_HISTORY_VERSION', '2.31.1');
+    }
 
-	if (!defined('SIMPLE_HISTORY_PATH')) {
-		define('SIMPLE_HISTORY_PATH', plugin_dir_path(__FILE__));
-	}
+    if (!defined('SIMPLE_HISTORY_PATH')) {
+        define('SIMPLE_HISTORY_PATH', plugin_dir_path(__FILE__));
+    }
 
-	if (!defined('SIMPLE_HISTORY_BASENAME')) {
-		define('SIMPLE_HISTORY_BASENAME', plugin_basename(__FILE__));
-	}
+    if (!defined('SIMPLE_HISTORY_BASENAME')) {
+        define('SIMPLE_HISTORY_BASENAME', plugin_basename(__FILE__));
+    }
 
-	if (!defined('SIMPLE_HISTORY_DIR_URL')) {
-		define('SIMPLE_HISTORY_DIR_URL', plugin_dir_url(__FILE__));
-	}
+    if (!defined('SIMPLE_HISTORY_DIR_URL')) {
+        define('SIMPLE_HISTORY_DIR_URL', plugin_dir_url(__FILE__));
+    }
 
-	if (!defined('SIMPLE_HISTORY_FILE')) {
-		define('SIMPLE_HISTORY_FILE', __FILE__);
-	}
+    if (!defined('SIMPLE_HISTORY_FILE')) {
+        define('SIMPLE_HISTORY_FILE', __FILE__);
+    }
 
-	/** Load required files */
-	require_once __DIR__ . '/inc/SimpleHistory.php';
-	require_once __DIR__ . '/inc/SimpleHistoryLogQuery.php';
+    /** Load required files */
+    require_once __DIR__ . '/inc/SimpleHistory.php';
+    require_once __DIR__ . '/inc/SimpleHistoryLogQuery.php';
 
-	/**
-	Constants will be like:
-	SIMPLE_HISTORY_FILE:
-	Var is string with length 57: /Users/username/GIT/Simple-History/index.php
-	SIMPLE_HISTORY_PATH:
-	Var is string with length 48: /Users/username/GIT/Simple-History/
-	SIMPLE_HISTORY_BASENAME:
-	Var is string with length 24: simple-history/index.php
-	*/
-
-	/** Boot up */
-	SimpleHistory::get_instance();
+    /** Boot up */
+    SimpleHistory::get_instance();
 } else {
-	// User is running to old version of php, add admin notice about that.
-	add_action('admin_notices', 'simple_history_old_version_admin_notice');
+    // User is running to old version of php, add admin notice about that.
+    add_action('admin_notices', 'simple_history_old_version_admin_notice');
 
-	/**
-	 * Show an admin message if old PHP version.
-	 */
-	function simple_history_old_version_admin_notice()
-	{
-		$ok_wp_version = version_compare($GLOBALS['wp_version'], '4.5.1', '>=');
-		$ok_php_version = version_compare(phpversion(), '5.3', '>=');
-		?>
+    /**
+     * Show an admin message if old PHP version.
+     */
+    function simple_history_old_version_admin_notice()
+    {
+        $ok_wp_version = version_compare($GLOBALS['wp_version'], '4.5.1', '>=');
+        $ok_php_version = version_compare(phpversion(), '5.3', '>=');
+        ?>
 		<div class="updated error">
 			<?php
    if (!$ok_php_version) {
-   	echo '<p>';
-   	printf(
-   		/* translators: 1: PHP version */
-   		esc_html(
-   			__(
-   				'Simple History is a great plugin, but to use it your server must have at least PHP 5.3 installed (you have version %s).',
-   				'simple-history'
-   			)
-   		),
-   		phpversion() // 1
-   	);
-   	echo '</p>';
+       echo '<p>';
+       printf(
+           /* translators: 1: PHP version */
+           esc_html(
+               __(
+                   'Simple History is a great plugin, but to use it your server must have at least PHP 5.3 installed (you have version %s).',
+                   'simple-history'
+               )
+           ),
+           phpversion() // 1
+       );
+       echo '</p>';
    }
 
    if (!$ok_wp_version) {
-   	echo '<p>';
-   	printf(
-   		/* translators: 1: WordPress version */
-   		esc_html(
-   			__(
-   				'Simple History requires WordPress version 4.5.1 or higher (you have version %s).',
-   				'simple-history'
-   			)
-   		),
-   		$GLOBALS['wp_version'] // 1
-   	);
-   	echo '</p>';
+       echo '<p>';
+       printf(
+           /* translators: 1: WordPress version */
+           esc_html(
+               __('Simple History requires WordPress version 4.5.1 or higher (you have version %s).', 'simple-history')
+           ),
+           $GLOBALS['wp_version'] // 1
+       );
+       echo '</p>';
    }?>
 		</div>
 		<?php
-	}
+    }
 } // End if().
