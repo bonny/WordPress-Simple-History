e130324efe08dd16d10f802e7d45e1f99c8ba050
Pär Thernström <par.thernstrom@gmail.com>
2019-05-21 22:57:34 +0200

    Format code in index

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
  * Plugin URI: http://simple-history.com
  * Text Domain: simple-history
  * Domain Path: /languages
- * Description: Plugin that logs various things that occur in WordPress and then
- *              presents those events in a very nice GUI.
+ * Description: Plugin that logs various things that occur in WordPress and then presents those events in a very nice GUI.
  * Version: 2.31.1
  * Author: Pär Thernström
  * Author URI: http://simple-history.com/
@@ -84,35 +83,38 @@ if ($ok_php_version && $ok_wp_version) {
         $ok_wp_version = version_compare($GLOBALS['wp_version'], '4.5.1', '>=');
         $ok_php_version = version_compare(phpversion(), '5.3', '>=');
         ?>
-		<div class="updated error">
-			<?php
-   if (!$ok_php_version) {
-       echo '<p>';
-       printf(
-           /* translators: 1: PHP version */
-           esc_html(
-               __(
-                   'Simple History is a great plugin, but to use it your server must have at least PHP 5.3 installed (you have version %s).',
-                   'simple-history'
-               )
-           ),
-           phpversion() // 1
-       );
-       echo '</p>';
-   }
+        <div class="updated error">
+            <?php
+            if (!$ok_php_version) {
+                echo '<p>';
+                printf(
+                    /* translators: 1: PHP version */
+                    esc_html(
+                        __(
+                            'Simple History is a great plugin, but to use it your server must have at least PHP 5.3 installed (you have version %s).',
+                            'simple-history'
+                        )
+                    ),
+                    phpversion() // 1
+                );
+                echo '</p>';
+            }
 
-   if (!$ok_wp_version) {
-       echo '<p>';
-       printf(
-           /* translators: 1: WordPress version */
-           esc_html(
-               __('Simple History requires WordPress version 4.5.1 or higher (you have version %s).', 'simple-history')
-           ),
-           $GLOBALS['wp_version'] // 1
-       );
-       echo '</p>';
-   }?>
-		</div>
-		<?php
+            if (!$ok_wp_version) {
+                echo '<p>';
+                printf(
+                    /* translators: 1: WordPress version */
+                    esc_html(
+                        __(
+                            'Simple History requires WordPress version 4.5.1 or higher (you have version %s).',
+                            'simple-history'
+                        )
+                    ),
+                    $GLOBALS['wp_version'] // 1
+                );
+                echo '</p>';
+            }?>
+        </div>
+        <?php
     }
 } // End if().
