11844b8ce459058686622645573e5dfca75e2514
Pär Thernström <par.thernstrom@gmail.com>
2020-08-29 20:18:11 +0200

    Remove check if constants are defined

--- index.php
+++ index.php
@@ -44,26 +44,11 @@ if ($ok_php_version && $ok_wp_version) {
      * @TODO: make activatigon multi site aware, as in https://github.com/scribu/wp-proper-network-activation
      * register_activation_hook( trailingslashit(WP_PLUGIN_DIR) . trailingslashit( plugin_basename(__DIR__) ) . "index.php" , array("SimpleHistory", "on_plugin_activate" ) );
      */
-
-    if (!defined('SIMPLE_HISTORY_VERSION')) {
-        define('SIMPLE_HISTORY_VERSION', '2.35.1');
-    }
-
-    if (!defined('SIMPLE_HISTORY_PATH')) {
-        define('SIMPLE_HISTORY_PATH', plugin_dir_path(__FILE__));
-    }
-
-    if (!defined('SIMPLE_HISTORY_BASENAME')) {
-        define('SIMPLE_HISTORY_BASENAME', plugin_basename(__FILE__));
-    }
-
-    if (!defined('SIMPLE_HISTORY_DIR_URL')) {
-        define('SIMPLE_HISTORY_DIR_URL', plugin_dir_url(__FILE__));
-    }
-
-    if (!defined('SIMPLE_HISTORY_FILE')) {
-        define('SIMPLE_HISTORY_FILE', __FILE__);
-    }
+    define('SIMPLE_HISTORY_VERSION', '2.35.1');
+    define('SIMPLE_HISTORY_PATH', plugin_dir_path(__FILE__));
+    define('SIMPLE_HISTORY_BASENAME', plugin_basename(__FILE__));
+    define('SIMPLE_HISTORY_DIR_URL', plugin_dir_url(__FILE__));
+    define('SIMPLE_HISTORY_FILE', __FILE__);
 
     /** Load required files */
     require_once __DIR__ . '/inc/SimpleHistory.php';
