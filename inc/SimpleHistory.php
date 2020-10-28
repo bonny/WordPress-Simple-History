044d41581fa86536e7d522edda97c90cd5087505
Clayton Collie <clayton.collie@gmail.com>
2020-10-28 11:59:34 -0500

    Remove unnecessary wording

--- inc/SimpleHistory.php
+++ inc/SimpleHistory.php
@@ -97,7 +97,7 @@ class SimpleHistory
         add_action('after_setup_theme', [$this, 'add_default_settings_tabs']);
 
         // Plugins and dropins are loaded using the "after_setup_theme" filter so
-        // themes and plugins can use filters to modify the loading of them.
+        // themes can use filters to modify the loading of them.
         // The drawback with this is that for example logouts done when plugins like
         // iThemes Security is installed is not logged, because those plugins fire wp_logout()
         // using filter "plugins_loaded", i.e. before simple history has loaded its filters.
