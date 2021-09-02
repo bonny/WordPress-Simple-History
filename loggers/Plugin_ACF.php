4d05efa36bc50dd25662c9ed545818fa7608853f
thegaffney <thegaffney@hotmail.com>
2021-09-01 21:09:40 -0700

    Update Plugin_ACF.php

--- loggers/Plugin_ACF.php
+++ loggers/Plugin_ACF.php
@@ -204,7 +204,7 @@ if (! class_exists('Plugin_ACF')) {
 
             $new_post_meta = get_post_custom($post_id);
             array_walk($new_post_meta, function(&$value, $key){
-				$value = reset($value);
+			    $value = reset($value);
             });
 
             // New and old post meta can contain different amount of keys,
@@ -529,7 +529,7 @@ if (! class_exists('Plugin_ACF')) {
 
             // Meta is array of arrays, get first value of each array value.
             array_walk($post_meta, function(&$value, $key){
-				$value = reset($value);
+			    $value = reset($value);
             });
 
             $this->oldPostData['prev_post_meta'] = $post_meta;
