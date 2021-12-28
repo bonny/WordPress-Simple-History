0058ece0abeacff910ea55de761297df8943d476
Pär Thernström <par.thernstrom@gmail.com>
2021-12-28 21:17:28 +0100

    Remove GitHub Plugin URI header from index

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
  * Author: Pär Thernström
  * Author URI: http://simple-history.com/
  * License: GPL2
- * GitHub Plugin URI: https://github.com/bonny/WordPress-Simple-History
+
  *
  * @package Simple History
  */
