0613fae6efa52c2b3e1ef76689342e0c96c083bf
Pär Thernström <par.thernstrom@gmail.com>
2022-01-13 21:43:08 +0100

    Add files for wp-browser testing

---
+++ tests/acceptance/FirstCest.php
@@ -0,0 +1,29 @@
+<?php
+
+class FirstCest {
+
+	public function visitPluginPage( AcceptanceTester $I ) {
+        $I->loginAsAdmin();
+        
+        // Kör inte filters etc. så loggas inte.
+        $I->haveUserInDatabase('erik', 'editor', ['user_pass' => 'password']);
+
+        $I->amOnAdminPage( 'index.php?page=simple_history_page' );
+        //$I->waitForElementVisible('.SimpleHistoryLogitemsWrap');
+        $I->see( 'Simple History' );
+        $I->see('Logged in', '.SimpleHistoryLogitem__text');
+        // $I->amOnPage('/');
+        //$I->makeHtmlSnapshot();
+
+        // Expanded search filters.
+        $I->click('Show search options');
+        $I->see('Log levels:');
+        $I->see('Message types:');
+        $I->see('Users:');
+
+        $I->loginAs('erik', 'password'); 
+
+        $I->makeScreenshot();
+        // $I->pause();
+	}
+}
---
+++ tests/unit/ExampleTest.php
@@ -0,0 +1,23 @@
+<?php
+
+class ExampleTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
