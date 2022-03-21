cbcaa06746b442e26e60c7d1915aba58d496093f
Pär Thernström <par.thernstrom@gmail.com>
2022-03-21 08:47:50 +0100

    More parameter formatting

--- docs/functions.js
+++ docs/functions.js
@@ -1,29 +1,56 @@
 export const HookParams = ({ hook }) => {
-  let params = hook.doc.tags.filter((tag) => tag.name === "param");
+  const params = hook.doc.tags.filter(tag => tag.name === 'param')
 
-  let paramsOutput = params.map((oneParam, idx, arr) => {
-    const paramTypesOut = oneParam.types.map((type) => {
-      return <span>{type}</span>;
-    });
+  const paramsOutput = params.map((oneParam, idx, arr) => {
+    const paramTypesOut = oneParam.types.map((type, typeIdx, typeArr) => {
+      const divider = typeIdx < typeArr.length - 1 ? '|' : ''
+      return (
+        <span>
+          {type}
+          {divider}
+        </span>
+      )
+    })
 
-    const comma = idx < arr.length - 1 ? ", " : "";
+    const comma = idx < arr.length ? ', ' : ''
 
     return (
       <span>
-        {paramTypesOut} {oneParam.variable}
         {comma}
+        {paramTypesOut} {oneParam.variable}
       </span>
-    );
-  });
+    )
+  })
 
-  const commaBeforeParams = paramsOutput.length ? ", " : "";
+  let paramsDoc = params.map((oneParam, idx, arr) => {
+    const paramTypesOut = oneParam.types.map((type, typeIdx, typeArr) => {
+      const divider = typeIdx < typeArr.length - 1 ? '|' : ''
+      return (
+        <span>
+          {type}
+          {divider}
+        </span>
+      )
+    })
+
+    return (
+      <li>
+        {paramTypesOut} {oneParam.variable} {oneParam.content}{' '}
+      </li>
+    )
+  })
+
+  if (paramsDoc) {
+    paramsDoc = <ul>{paramsDoc}</ul>
+  }
 
   return (
     <div>
       <code>
-        do_action( '{hook.name}'{commaBeforeParams}
-        {paramsOutput} )
+        do_action( '{hook.name}'{paramsOutput} )
       </code>
+
+      <div>{paramsDoc}</div>
     </div>
-  );
-};
+  )
+}
