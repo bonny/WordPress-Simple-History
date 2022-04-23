71d9e2fd0d47a98ff4287761b7d25b9332b80e9e
Pär Thernström <par.thernstrom@gmail.com>
2022-04-23 22:15:20 +0200

    Add GA

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -1,7 +1,48 @@
-import "../styles.css";
+import '../styles.css'
 import 'nextra-theme-docs/style.css'
+import { useEffect } from 'react'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 export default function Nextra({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => {
+      console.log('handleRouteChange', url)
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   const getLayout = Component.getLayout || (page => page)
-  return getLayout(<Component {...pageProps} />)
+
+  return getLayout(
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
