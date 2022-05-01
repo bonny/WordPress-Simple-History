88090452860e09dd5c276f8ebc924b4463a2a5a8
Pär Thernström <par.thernstrom@gmail.com>
2022-05-01 15:25:14 +0200

    Update docs

--- docs/lib/LogItem.js
+++ docs/lib/LogItem.js
@@ -2,10 +2,11 @@ import Image from 'next/image'
 import profilePics from './profile-images/'
 
 function LogItem({
-  who = 'Jane Does',
+  who = 'Jane Doe',
   date = 'April 5, 2022 - 22:23 (25 days ago)',
   text = '',
-  level = 'info'
+  level = 'info',
+  similarCount = 0
 }) {
   const Loglevel = function () {
     if (level === 'info') {
@@ -46,7 +47,7 @@ function LogItem({
     let classes = 'rounded text-xs px-1.5 py-1 ml-2 relative -top-.5'
     let styles = {
       color: colors[level].color,
-      'background-color': colors[level].bgcolor
+      backgroundColor: colors[level].bgcolor
     }
 
     return (
@@ -56,17 +57,26 @@ function LogItem({
     )
   }
 
-  const randomProfilePictureNumber = Math.floor(
-    Math.random() * profilePics.length
-  )
-  const profilePic = profilePics[randomProfilePictureNumber]
+  const profilePic = profilePics[Math.floor(Math.random() * profilePics.length)]
+
+  const SimilarEvents = function () {
+    if (!similarCount) {
+      return null
+    }
+
+    return (
+      <p className="text-blue-500 text-sm" style={{ marginTop: '.25rem' }}>
+        +{similarCount} similar events
+      </p>
+    )
+  }
 
   return (
     <div className="bg-gray-200 p-4 rounded-lg">
       <div className="bg-white p-4 rounded-md">
         <div className="flex gap-5">
           <div>
-            <p className="w-12 text-blue-500">
+            <p className="w-12 text-blue-600">
               <Image src={profilePic} layout="responsive" />
             </p>
           </div>
@@ -74,12 +84,13 @@ function LogItem({
             <p className="m-0 text-sm">
               <span className="font-semibold">{who}</span>
               <span className="text-gray-400"> • </span>
-              <span className="text-sky-700">{date}</span>
+              <span className="text-blue-600">{date}</span>
             </p>
             <p className="m-0 text-lg" style={{ marginTop: '.25rem' }}>
               {text}
               <Loglevel />
             </p>
+            <SimilarEvents />
           </div>
         </div>
       </div>
