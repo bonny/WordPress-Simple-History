c71d1dac007d239980cf4817ca9044218a22071a
Pär Thernström <par.thernstrom@gmail.com>
2022-05-08 11:38:56 +0200

    Add Rector and set PHP version to 7

---
+++ rector.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function ( RectorConfig $rectorConfig ): void {
+	$rectorConfig->paths(
+		array(
+			__DIR__ . '/loggers',
+			__DIR__ . '/dropins',
+			__DIR__ . '/examples',
+			__DIR__ . '/inc',
+			__DIR__ . '/templates',
+		)
+	);
+
+	$rectorConfig->phpVersion( PhpVersion::PHP_70 );
+
+	// register a single rule
+	// $rectorConfig->rule( InlineConstructorDefaultToPropertyRector::class );
+
+	// define sets of rules
+	$rectorConfig->sets(
+		array(
+			LevelSetList::UP_TO_PHP_70,
+		)
+	);
+};
--- tests/readme.md
+++ tests/readme.md
@@ -12,12 +12,6 @@ Tests use [wpbrowser](https://wpbrowser.wptestkit.dev/).
   - On _PHP 7.4_:
     - `$ docker-compose run --rm php-cli vendor/bin/codecept run wpunit`
     - `$ docker-compose run --rm php-cli vendor/bin/codecept run acceptance`
-  - On _PHP 5.6_:
-    - `$ PHP_CLI_VERSION=56 docker-compose run --rm php-cli vendor/bin/codecept run wpunit`
-    - For acceptance tests wordpress-container must be restarted:
-      - `$ WORDPRESS_VERSION=5 PHP_VERSION=5.6 docker-compose up`
-      - `$ docker-compose run --rm php-cli vendor/bin/codecept run acceptance`
-
 ## Run a single test
 
 To run for example `UserCest:logUserProfileUpdated`:
