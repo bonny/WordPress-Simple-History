36c35c979db5095be187fc83364b4b0082813ea6
Pär Thernström <par.thernstrom@gmail.com>
2022-05-09 09:03:22 +0200

    Update Rector config

--- rector.php
+++ rector.php
@@ -6,6 +6,9 @@ use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector;
 use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
 use Rector\CodeQuality\Rector\Array_\ArrayThisCallToThisMethodCallRector;
+use Rector\DeadCode\Rector\MethodCall\RemoveEmptyMethodCallRector;
+use Rector\Renaming\Rector\FuncCall\RenameFunctionRector;
+use Rector\CodeQuality\Rector\If_\SimplifyIfReturnBoolRector;
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\LevelSetList;
@@ -18,7 +21,7 @@ return static function ( RectorConfig $rectorConfig ): void {
 		)
 	);
 
-	$rectorConfig->phpVersion( PhpVersion::PHP_70 );
+	$rectorConfig->phpVersion( PhpVersion::PHP_74 );
 
 	// register a single rule
 	// $rectorConfig->rule( InlineConstructorDefaultToPropertyRector::class );
@@ -33,10 +36,15 @@ return static function ( RectorConfig $rectorConfig ): void {
 			__DIR__ . '/rector.php',
 			// Disable rule because I like to concat my strings.
 			SimplifyUselessVariableRector::class,
-			// Doesn't feel WordPress'ish
+			// Doesn't feel WordPress'ish.
 			CallableThisArrayToAnonymousFunctionRector::class,
-			// Looks wierd
-			ArrayThisCallToThisMethodCallRector::class
+			// Looks wierd.
+			ArrayThisCallToThisMethodCallRector::class,
+			// Makes to code more difficult to read.
+			SimplifyIfReturnBoolRector::class,
+			// Need to find out what they do
+			RemoveEmptyMethodCallRector::class,
+			RenameFunctionRector::class,
 		)
 	);
 
@@ -45,7 +53,7 @@ return static function ( RectorConfig $rectorConfig ): void {
 	// https://github.com/rectorphp/rector-src/blob/main/rector.php
 	$rectorConfig->sets(
 		array(
-			LevelSetList::UP_TO_PHP_70,
+			LevelSetList::UP_TO_PHP_74,
 			SetList::DEAD_CODE,
 			SetList::CODE_QUALITY,
 			// Add lists one by one to make diffs not to big.
@@ -54,3 +62,11 @@ return static function ( RectorConfig $rectorConfig ): void {
 	);
 
 };
+
+/*
+Random todo som har med PHP 7.4 att göra:
+- Ta bort ca. version_compare( PHP_VERSION, '5.4.0' )
+- Flytta ut kod ca "get user role, as done in user-edit.php" till egen funktion
+- Få igång tester så vi kan testa enkelt.
+- Göra dold sida som visar loggen utan ajax, borde gå snabbare att använda för testerna som slipper vänta på ajax.
+*/
