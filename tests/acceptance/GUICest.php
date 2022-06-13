92f90eb8c88219fee7b9ab9133ef030da3523c70
Pär Thernström <par.thernstrom@gmail.com>
2022-06-13 21:59:18 +0200

    Add gui aacceptance test

--- tests/acceptance/GUICest.php
+++ tests/acceptance/GUICest.php
@@ -22,6 +22,15 @@ class GUICest {
                 $I->see('Message types:');
                 $I->see('Users:');
 
+                // Sidebar boxes.
+                $I->see('Stats');
+                $I->see('Number of events per day.');
+                $I->see('Settings');
+                $I->see('Visit the settings page');
+                $I->see('Review this plugin if you like it');
+                $I->see('Visit the support forum');
+                $I->see('Donate to support development');
+
                 $I->loginAs('erik', 'password'); 
                 
                 $I->amOnAdminPage( 'index.php?page=simple_history_page' );
