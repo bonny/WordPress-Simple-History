6b62ba7ad20c7adee2dd0e6f063962232eb686a9
Pär Thernström <par.thernstrom@gmail.com>
2022-06-24 18:41:08 +0200

    Test wp crontrol add schedule

--- tests/acceptance/PluginWPCrontrolLoggerCest.php
+++ tests/acceptance/PluginWPCrontrolLoggerCest.php
@@ -9,5 +9,14 @@ class PluginWPCrontrolLoggerCest
         $I->amOnPluginsPage();
         $I->activatePlugin('wp-crontrol');
         $I->canSeePluginActivated('wp-crontrol');
+        // "Cron Schedules" tab.
+        $I->amOnAdminPage( 'options-general.php?page=crontrol_admin_options_page' );
+        
+        // Add a schedule, message key "added_new_event".
+        $I->fillField("#crontrol_schedule_internal_name", "my_123_seconds_interval");
+        $I->fillField("#crontrol_schedule_interval", 123);
+        $I->fillField("#crontrol_schedule_display_name", "Every 123 seconds");
+        $I->click('Add Cron Schedule');
+        $I->seeLogMessage('Added cron schedule "my_123_seconds_interval"');
     }
 }
