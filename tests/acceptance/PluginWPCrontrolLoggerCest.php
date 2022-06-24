15831b60e8d577e68b9e7caa021a56e1c533a23f
Pär Thernström <par.thernstrom@gmail.com>
2022-06-24 18:41:25 +0200

    Test wp crontrol manually run schedule

--- tests/acceptance/PluginWPCrontrolLoggerCest.php
+++ tests/acceptance/PluginWPCrontrolLoggerCest.php
@@ -9,6 +9,7 @@ class PluginWPCrontrolLoggerCest
         $I->amOnPluginsPage();
         $I->activatePlugin('wp-crontrol');
         $I->canSeePluginActivated('wp-crontrol');
+        
         // "Cron Schedules" tab.
         $I->amOnAdminPage( 'options-general.php?page=crontrol_admin_options_page' );
         
@@ -18,5 +19,18 @@ class PluginWPCrontrolLoggerCest
         $I->fillField("#crontrol_schedule_display_name", "Every 123 seconds");
         $I->click('Add Cron Schedule');
         $I->seeLogMessage('Added cron schedule "my_123_seconds_interval"');
+
+        // "Cron Events" tab.
+        $I->amOnAdminPage('tools.php?page=crontrol_admin_manage_page');
+        
+        // Manually run an event. Message key 'ran_event'.
+        // Manually run cron "wp_version_check" by clicking link "Run now" for that cron.
+        // HTML is like this: <a href="http://wordpress-stable.test/wp-admin/tools.php?page=crontrol_admin_manage_page&amp;
+
+        // Add class .visible to div.row-actions to make link visible.
+        $I->executeJS("jQuery('div.row-actions').addClass('visible');");
+        // Click "Run now" link.
+        $I->click('a[href*="crontrol_action=run-cron"][href*=version]');
+        $I->seeLogMessage('Manually ran cron event "wp_version_check"');
     }
 }
