9b575f2d55b32320855b5f199de907f19622cac3
Pär Thernström <par.thernstrom@gmail.com>
2022-06-24 20:17:27 +0200

    Test wp crontrol delete schedule

--- tests/acceptance/PluginWPCrontrolLoggerCest.php
+++ tests/acceptance/PluginWPCrontrolLoggerCest.php
@@ -4,22 +4,32 @@ use \Step\Acceptance\Admin;
 
 class PluginWPCrontrolLoggerCest
 {
-    public function activatePlugin(Admin $I) {
+    public function _before(Admin $I) {
         $I->loginAsAdmin();
         $I->amOnPluginsPage();
         $I->activatePlugin('wp-crontrol');
-        $I->canSeePluginActivated('wp-crontrol');
-        
+        $I->canSeePluginActivated('wp-crontrol');        
+    }
+    
+    public function addScheduleAndDeleteSchedule(Admin $I) {
         // "Cron Schedules" tab.
         $I->amOnAdminPage( 'options-general.php?page=crontrol_admin_options_page' );
         
-        // Add a schedule, message key "added_new_event".
+        // Add a schedule, message key "added_new_schedule".
         $I->fillField("#crontrol_schedule_internal_name", "my_123_seconds_interval");
         $I->fillField("#crontrol_schedule_interval", 123);
         $I->fillField("#crontrol_schedule_display_name", "Every 123 seconds");
         $I->click('Add Cron Schedule');
         $I->seeLogMessage('Added cron schedule "my_123_seconds_interval"');
 
+        // Delete a schedule. Message key "deleted_schedule".
+        $I->executeJS("jQuery('div.row-actions').addClass('visible');");
+        // <a href="http://wordpress-stable.test/wp-admin/options-general.php?page=crontrol_admin_options_page&amp;crontrol_action=delete-schedule&amp;crontrol_id=my_123_seconds_interval&amp;_wpnonce=07b671650d">Delete</a>
+        $I->click('a[href*="crontrol_action=delete-schedule"][href*="crontrol_id=my_123_seconds_interval"]');
+        $I->seeLogMessage('Deleted cron schedule "my_123_seconds_interval"');
+    }
+
+    public function manuallyRunCronEvent(Admin $I) {
         // "Cron Events" tab.
         $I->amOnAdminPage('tools.php?page=crontrol_admin_manage_page');
         
