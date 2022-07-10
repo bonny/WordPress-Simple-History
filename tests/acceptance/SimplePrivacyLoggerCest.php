94f94fdeea510172f949d14631a09e09a5d7994d
Pär Thernström <par.thernstrom@gmail.com>
2022-07-10 11:09:22 +0200

    Test personal data erasure

--- tests/acceptance/SimplePrivacyLoggerCest.php
+++ tests/acceptance/SimplePrivacyLoggerCest.php
@@ -4,7 +4,6 @@ use \Step\Acceptance\Admin;
 /**
  * 'privacy_data_export_emailed'           => _x( 'Sent email with personal data export download info for user "{user_email}"', 'Logger: Privacy', 'simple-history' ),
  * 'privacy_data_export_request_confirmed' => _x( 'Confirmed data export request for "{user_email}"', 'Logger: Privacy', 'simple-history' ),
- * 'privacy_data_export_removed'           => _x( 'Removed data export request for "{user_email}"', 'Logger: Privacy', 'simple-history' ),
  * 'data_erasure_request_sent'             => _x( 'Sent data erasure request for "{user_email}"', 'Logger: Privacy', 'simple-history' ),
  * 'data_erasure_request_confirmed'        => _x( 'Confirmed data erasure request for "{user_email}"', 'Logger: Privacy', 'simple-history' ),
  * 'data_erasure_request_handled'          => _x( 'Erased personal data for "{user_email}"', 'Logger: Privacy', 'simple-history' ),
@@ -57,13 +56,10 @@ class SimplePrivacyLoggerCest
         $I->seeLogMessage('Set privacy page to page "My new privacy page"');
     }
 
-    /**
-     * Go to export personal data page and add data export request.
-     *
-     * Message key: privacy_data_export_requested
-     */
     public function logDataExportRequest(Admin $I)
     {
+        // Message key: privacy_data_export_requested.
+        // Go to export personal data page and add data export request.
         $I->haveUserInDatabase('myNewUser');
         
         $I->loginAsAdmin();        
@@ -114,5 +110,52 @@ class SimplePrivacyLoggerCest
         $I->moveMouseOver('.table-view-list tbody tr:nth-child(1)');
         $I->click('Remove request');
         $I->seeLogMessage('Removed data export request for "myNewUser@example.com"');
+    }
+
+    /**
+     * Tools > Erase Personal Data
+     */
+    public function logDataErasure(Admin $I)
+    {
+        $I->haveUserInDatabase('myNewUser');
+
+        // data_erasure_request_added
+        $I->loginAsAdmin();        
+        $I->amOnAdminPage('erase-personal-data.php');
+        $I->fillField('#username_or_email_for_privacy_request', 'myNewUser');
+        $I->click('Send Request');
+
+        $I->seeLogInitiator('wp_user');
+        $I->seeLogMessage('Added personal data erasure request for "myNewUser@example.com"');
+        $I->seeLogContext([
+            'send_confirmation_email' => 1,
+        ]);
+
+        // data_erasure_request_completed
+        $I->amOnAdminPage('erase-personal-data.php');
+        $I->moveMouseOver('.table-view-list tbody tr:nth-child(1)');
+        $I->click('Complete request');
+        $I->seeLogMessage('Marked personal data erasure request as complete for "myNewUser@example.com"');
+               
+        // data_erasure_request_removed
+        /*
+        action: wp-privacy-erase-personal-data
+        eraser: 1
+        id: 74
+        page: 1
+        security: c22f7c8669
+        */
+        $I->amOnAdminPage('erase-personal-data.php');
+        $I->moveMouseOver('.table-view-list tbody tr:nth-child(1)');
+        $I->click('Force erase personal data');
+        $I->waitForText('Erasure complete');
+        $I->seeLogMessage('Erased personal data for "myNewUser@example.com"');
+
+        // data_erasure_request_removed
+        $I->amOnAdminPage('erase-personal-data.php');
+        $I->moveMouseOver('.table-view-list tbody tr:nth-child(1)');
+        $I->click('Remove request');
+        $I->seeLogMessage('Removed personal data removal request for "myNewUser@example.com"');
+        
     }
 }
