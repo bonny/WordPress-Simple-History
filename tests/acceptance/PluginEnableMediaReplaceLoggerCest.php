ccfa9291adb8238a042414bc8c0c244a797e78e8
Pär Thernström <par.thernstrom@gmail.com>
2022-08-17 20:05:29 +0200

    Add tests for plugin Enable Media Replace

---
+++ tests/acceptance/PluginEnableMediaReplaceLoggerCest.php
@@ -0,0 +1,39 @@
+<?php
+
+use \Step\Acceptance\Admin;
+
+class EnableMediaReplaceLoggerCest
+{
+    public function _before(Admin $I) {
+        $plugin_slug = 'enable-media-replace';
+        $I->loginAsAdmin();
+        $I->amOnPluginsPage();
+        $I->activatePlugin($plugin_slug);
+        $I->canSeePluginActivated($plugin_slug);
+    }
+
+    public function replaceImage(Admin $I) {
+        // Upload first image.
+        $I->amOnAdminPage('/media-new.php');
+        $I->click("browser uploader");
+        $I->attachFile('#async-upload', 'Image 1.jpg');
+        $I->click("Upload");
+        $I->seeLogMessage('Created attachment "Image 1"');
+        
+        // Upload second image, that replaces first image.
+        $I->amOnAdminPage('upload.php?mode=list');
+        $I->click('Image 1');
+        $I->click('Upload a new file');
+        $I->attachFile('#userfile', 'Image 2.jpg');
+        $I->click('Upload');
+        $I->makeScreenshot();
+        $I->seeLogMessage('Replaced attachment "Image 1" with new attachment "Image 2.jpg"');        
+        $I->seeLogContext([
+            'prev_attachment_title' => 'Image 1',
+            'new_attachment_title' => 'Image 2.jpg',
+            'new_attachment_type' => 'image/jpeg',
+            'new_attachment_size' => '586250',
+            'replace_type' => 'replace',
+        ]);
+    }
+}
