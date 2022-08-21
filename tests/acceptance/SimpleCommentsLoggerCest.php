cf0c03d60c42e28fdd13d603cd7f5e7e40d46e17
Pär Thernström <par.thernstrom@gmail.com>
2022-08-21 16:19:12 +0200

    Add tests for comments logger

---
+++ tests/acceptance/SimpleCommentsLoggerCest.php
@@ -0,0 +1,50 @@
+<?php
+
+use \Step\Acceptance\Admin;
+
+/*
+*/
+
+class SimpleCommentsLoggerCest
+{
+    public function _before(Admin $I) {
+        $I->loginAsAdmin();
+    }
+
+    public function editComment(Admin $I) {
+        $post_id = $I->havePostInDatabase([
+            'post_title' => 'My test post'
+        ]);
+        $I->haveManyCommentsInDatabase(1, $post_id);
+
+        // Unapprove.
+        $I->amOnAdminPage('edit-comments.php');
+        $I->moveMouseOver('.wp-list-table tbody tr:nth-child(1)');
+        $I->click('Unapprove');
+        $I->waitForJqueryAjax();
+        $I->seeLogMessage('Unapproved a comment to "My test post" by Mr WordPress ()');
+
+        // Approve.
+        $I->click('Approve');
+        $I->waitForJqueryAjax();
+        $I->seeLogMessage('Approved a comment to "My test post" by Mr WordPress ()');
+        
+        // Trash.
+        $I->click('Trash');
+        $I->waitForJqueryAjax();
+        $I->seeLogMessage('Trashed a comment to "My test post" by Mr WordPress ()');
+
+        // Untrash.
+        $I->amOnAdminPage('edit-comments.php?comment_status=trash');
+        $I->moveMouseOver('.wp-list-table tbody tr:nth-child(1)');
+        $I->click('Restore');
+        $I->waitForJqueryAjax();
+        $I->seeLogMessage('Restored a comment to "My test post" by Mr WordPress () from the trash');
+
+        // Trash and Delete permanently.
+        
+
+        $I->makeScreenshot();
+
+    }
+}
