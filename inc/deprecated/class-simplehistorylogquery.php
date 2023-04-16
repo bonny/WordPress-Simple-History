d27449b5579b8026aa9687939f832f1af088804a
Pär Thernström <par.thernstrom@gmail.com>
2023-04-16 17:49:03 +0200

    Add fallback class SimpleHistoryLogQuery

--- inc/deprecated/class-simplehistorylogquery.php
+++ inc/deprecated/class-simplehistorylogquery.php
@@ -1,9 +1,21 @@
 <?php
 
+use Simple_History\Log_Query;
+
 /**
  * Deprecated, use Simple_History\Log_Query instead.
- * @package
+ *
+ * Un-namespaced class for old loggers that call \SimpleHistoryLogQuery.
  */
 class SimpleHistoryLogQuery {
-
+	/**
+	 * Only function "query" exists on old class.
+	 *
+	 * @since 4.0
+	 * @param array $args
+	 * @return array
+	 */
+	public function query( $args ) {
+		return ( new Log_Query() )->query( $args );
+	}
 }
