825300409abafd5c3fc9538b7d9d36bd57129e92
Pär Thernström <par.thernstrom@gmail.com>
2023-06-12 16:13:09 +0200

    Comment

--- loggers/Logger.php
+++ loggers/Logger.php
@@ -15,9 +15,9 @@ use Simple_History\Helpers;
  * A PSR-3 inspired logger class.
  * This class logs + formats logs for display in the Simple History GUI/Viewer.
  *
- * Extend this class to make your own logger
+ * Extend this class to make your own logger.
  *
- * @link https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-3-logger-interface.md PSR-3 specification
+ * @link https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-3-logger-interface.md PSR-3 specification.
  */
 abstract class Logger {
 	/**
