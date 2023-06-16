9c7f0c73308cb8135368129fa2eae9b92e2f64e1
Pär Thernström <par.thernstrom@gmail.com>
2023-06-16 09:05:09 +0200

    Update Jetpack tests

--- tests/acceptance/JetpackLoggerCest.php
+++ tests/acceptance/JetpackLoggerCest.php
@@ -3,21 +3,23 @@ use \Step\Acceptance\Admin;
 
 class JetpackLoggerCest
 {
-    public function activatePlugin(Admin $I) {
+    public function _before(Admin $I) {
         $I->loginAsAdmin();
         $I->amOnPluginsPage();
         $I->activatePlugin('jetpack');
         $I->canSeePluginActivated('jetpack');
-        
+    }
+
+    public function test_that_jetpack_modules_can_be_activated(Admin $I) {        
         $I->amOnAdminPage( 'admin.php?page=jetpack#/performance' );
 
         // Enable site accelerator.
-        $I->click('label[for="toggle-0"]');
-        $I->wait(1); // Toggle takes some time to be activated.        
+        $I->click('#inspector-toggle-control-0');
+        $I->wait(1); // Toggle takes some time to be activated.    
         $I->seeLogMessage('Activated Jetpack module "Asset CDN"');
 
         // Enable Lazy Loading for images.
-        $I->click('label[for="toggle-3"]');
+        $I->click('#inspector-toggle-control-3');
         $I->wait(1); // Toggle takes some time to be activated.        
         $I->seeLogMessage('Activated Jetpack module "Lazy Images"');
     }
