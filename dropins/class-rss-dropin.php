421d63fbac5eaa26ac2583a3e1a4695bb06a70bc
Pär Thernström <par.thernstrom@gmail.com>
2023-08-08 15:43:02 +0200

    Feeds: Add link to documentation for RSS feed arguments

--- dropins/class-rss-dropin.php
+++ dropins/class-rss-dropin.php
@@ -432,6 +432,21 @@ class RSS_Dropin extends Dropin {
 			</p>',
 			esc_url( $this->get_rss_address() )
 		);
+
+		echo '<p>';
+		echo wp_kses(
+			sprintf(
+				/* translators: %s is a link to the documentation */
+				__( 'You can control what to include in the feed using query parameters. <a href="%1$s">View documentation</a>.', 'simple-history' ),
+				'https://simple-history.com/docs/feeds/?utm_source=wpadmin'
+			),
+			[
+				'a' => [
+					'href' => [],
+				],
+			]
+		);
+		echo '</p>';
 	}
 
 	/**
