d26b668fc9b45240cf09b8ca8495549fa91e22b0
Pär Thernström <par.thernstrom@gmail.com>
2023-08-12 08:41:07 +0200

    Move Ajax API to service

--- inc/class-simple-history.php
+++ inc/class-simple-history.php
@@ -115,6 +115,7 @@ class Simple_History {
 			Services\Setup_Log_Filters::class,
 			Services\Setup_Pause_Resume_Actions::class,
 			Services\Setup_Purge_DB_Cron::class,
+			Services\API::class,
 		];
 	}
 
@@ -140,22 +141,13 @@ class Simple_History {
 	 * @since 2.5.2
 	 */
 	private function add_admin_actions() {
-
 		add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
-
 		add_action( 'admin_footer', array( $this, 'add_js_templates' ) );
-
 		add_action( 'wp_dashboard_setup', array( $this, 'add_dashboard_widget' ) );
-
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
-
 		add_action( 'admin_head', array( $this, 'on_admin_head' ) );
 		add_action( 'admin_footer', array( $this, 'on_admin_footer' ) );
-
-		add_action( 'wp_ajax_simple_history_api', array( $this, 'api' ) );
-
 		add_filter( 'plugin_action_links_simple-history/index.php', array( $this, 'plugin_action_links' ), 10, 4 );
-
 		add_action( 'admin_bar_menu', array( $this, 'add_admin_bar_network_menu_item' ), 40 );
 		add_action( 'admin_bar_menu', array( $this, 'add_admin_bar_menu_item' ), 40 );
 	}
@@ -438,84 +430,6 @@ class Simple_History {
 				$one_logger['instance']->admin_js();
 			}
 		}
-	}
-
-	// TODO: move api to own class, inject simple history instance.
-	/**
-	 * Base url is:
-	 * /wp-admin/admin-ajax.php?action=simple_history_api
-	 *
-	 * Examples:
-	 * http://playground-root.ep/wp-admin/admin-ajax.php?action=simple_history_api&posts_per_page=5&paged=1&format=html
-	 */
-	public function api() {
-		// Fake slow answers
-		// sleep(2);
-		// sleep(rand(0,3));
-		$args = $_GET;
-		unset( $args['action'] );
-
-		// Type = overview | ...
-		$type = $_GET['type'] ?? null;
-
-		if ( empty( $args ) || ! $type ) {
-			wp_send_json_error(
-				array(
-					_x( 'Not enough args specified', 'API: not enough arguments passed', 'simple-history' ),
-				)
-			);
-		}
-
-		// User must have capability to view the history page
-		if ( ! current_user_can( $this->get_view_history_capability() ) ) {
-			wp_send_json_error(
-				array(
-					'error' => 'CAPABILITY_ERROR',
-				)
-			);
-		}
-
-		if ( isset( $args['id'] ) ) {
-			$args['post__in'] = array( $args['id'] );
-		}
-
-		$data = [];
-
-		switch ( $type ) {
-			case 'overview':
-			case 'occasions':
-			case 'single':
-				// API use SimpleHistoryLogQuery, so simply pass args on to that
-				$logQuery = new Log_Query();
-
-				$data = $logQuery->query( $args );
-
-				$data['api_args'] = $args;
-
-				// Output can be array or HTML
-				if ( isset( $args['format'] ) && 'html' === $args['format'] ) {
-					$data['log_rows_raw'] = [];
-
-					foreach ( $data['log_rows'] as $key => $oneLogRow ) {
-						$args = [];
-						if ( $type == 'single' ) {
-							$args['type'] = 'single';
-						}
-
-						$data['log_rows'][ $key ] = $this->get_log_row_html_output( $oneLogRow, $args );
-					}
-
-					$data['num_queries'] = get_num_queries();
-					$data['cached_result'] = $data['cached_result'] ?? false;
-				}
-
-				break;
-
-			default:
-				$data[] = 'Nah.';
-		} // End switch().
-
-		wp_send_json_success( $data );
 	}
 
 	/**
---
+++ inc/services/class-api.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Simple_History\Services;
+
+use Simple_History\Log_Query;
+
+/**
+ * Class for core services to extend,
+ * i.e. services that are loaded early and are required for Simple History to work.
+ */
+class API extends Service {
+	public function loaded() {
+		add_action( 'wp_ajax_simple_history_api', array( $this, 'api' ) );
+	}
+
+	/**
+	 * Base url is:
+	 * /wp-admin/admin-ajax.php?action=simple_history_api
+	 *
+	 * Examples:
+	 * http://playground-root.ep/wp-admin/admin-ajax.php?action=simple_history_api&posts_per_page=5&paged=1&format=html
+	 */
+	public function api() {
+		// Fake slow answers
+		// sleep(2);
+		// sleep(rand(0,3));
+		$args = $_GET;
+		unset( $args['action'] );
+
+		// Type = overview | ...
+		$type = $_GET['type'] ?? null;
+
+		if ( empty( $args ) || ! $type ) {
+			wp_send_json_error(
+				array(
+					_x( 'Not enough args specified', 'API: not enough arguments passed', 'simple-history' ),
+				)
+			);
+		}
+
+		// User must have capability to view the history page
+		if ( ! current_user_can( $this->simple_history->get_view_history_capability() ) ) {
+			wp_send_json_error(
+				array(
+					'error' => 'CAPABILITY_ERROR',
+				)
+			);
+		}
+
+		if ( isset( $args['id'] ) ) {
+			$args['post__in'] = array( $args['id'] );
+		}
+
+		$data = [];
+
+		switch ( $type ) {
+			case 'overview':
+			case 'occasions':
+			case 'single':
+				// API use SimpleHistoryLogQuery, so simply pass args on to that
+				$logQuery = new Log_Query();
+
+				$data = $logQuery->query( $args );
+
+				$data['api_args'] = $args;
+
+				// Output can be array or HTML.
+				if ( isset( $args['format'] ) && 'html' === $args['format'] ) {
+					$data['log_rows_raw'] = [];
+
+					foreach ( $data['log_rows'] as $key => $oneLogRow ) {
+						$args = [];
+						if ( $type == 'single' ) {
+							$args['type'] = 'single';
+						}
+
+						$data['log_rows'][ $key ] = $this->simple_history->get_log_row_html_output( $oneLogRow, $args );
+					}
+
+					$data['num_queries'] = get_num_queries();
+					$data['cached_result'] = $data['cached_result'] ?? false;
+				}
+
+				break;
+
+			default:
+				$data[] = 'Nah.';
+		} // End switch().
+
+		wp_send_json_success( $data );
+	}
+
+}
