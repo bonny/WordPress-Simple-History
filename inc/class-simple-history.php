04223eb304672128176e5109b9dcfe9d2adbf719
Pär Thernström <par.thernstrom@gmail.com>
2023-08-13 12:02:00 +0200

    Move network menu items to service

--- inc/class-simple-history.php
+++ inc/class-simple-history.php
@@ -105,7 +105,7 @@ class Simple_History {
 	 *
 	 * @return array<string> Array with classnames.
 	 */
-	private function get_core_services() {
+	private function get_services() {
 		return [
 			Services\Language_Loader::class,
 			Services\Setup_Database::class,
@@ -117,6 +117,7 @@ class Simple_History {
 			Services\Setup_Purge_DB_Cron::class,
 			Services\API::class,
 			Services\Dashboard_Widget::class,
+			Services\Network_Menu_Items::class,
 		];
 	}
 
@@ -124,7 +125,7 @@ class Simple_History {
 	 * Load services that are required for Simple History to work.
 	 */
 	private function load_services() {
-		foreach ( $this->get_core_services() as $service_classname ) {
+		foreach ( $this->get_services() as $service_classname ) {
 			$this->load_service( $service_classname );
 		}
 	}
@@ -148,131 +149,6 @@ class Simple_History {
 		add_action( 'admin_head', array( $this, 'on_admin_head' ) );
 		add_action( 'admin_footer', array( $this, 'on_admin_footer' ) );
 		add_filter( 'plugin_action_links_simple-history/index.php', array( $this, 'plugin_action_links' ), 10, 4 );
-		add_action( 'admin_bar_menu', array( $this, 'add_admin_bar_network_menu_item' ), 40 );
-		add_action( 'admin_bar_menu', array( $this, 'add_admin_bar_menu_item' ), 40 );
-	}
-
-	/**
-	 * Adds a "View history" item/shortcut to the network admin, on blogs where Simple History is installed
-	 *
-	 * Useful because Simple History is something at least the author of this plugin often use on a site :)
-	 *
-	 * @since 2.7.1
-	 */
-	public function add_admin_bar_network_menu_item( $wp_admin_bar ) {
-		/**
-		 * Filter to control if admin bar shortcut should be added
-		 *
-		 * @since 2.7.1
-		 *
-		 * @param bool $add_item Add item
-		 */
-		$add_item = apply_filters( 'simple_history/add_admin_bar_network_menu_item', true );
-
-		if ( ! $add_item ) {
-			return;
-		}
-
-		// Don't show for logged out users or single site mode.
-		if ( ! is_user_logged_in() || ! is_multisite() ) {
-			return;
-		}
-
-		// Show only when the user has at least one site, or they're a super admin.
-		if ( ( is_countable( $wp_admin_bar->user->blogs ) ? count( $wp_admin_bar->user->blogs ) : 0 ) < 1 && ! is_super_admin() ) {
-			return;
-		}
-
-		// Setting to show as page must be true
-		if ( ! $this->setting_show_as_page() ) {
-			return;
-		}
-
-		// User must have capability to view the history page
-		if ( ! current_user_can( $this->get_view_history_capability() ) ) {
-			return;
-		}
-
-		foreach ( (array) $wp_admin_bar->user->blogs as $blog ) {
-			switch_to_blog( $blog->userblog_id );
-
-			if ( Helpers::is_plugin_active( SIMPLE_HISTORY_BASENAME ) ) {
-				$menu_id = 'simple-history-blog-' . $blog->userblog_id;
-				$parent_menu_id = 'blog-' . $blog->userblog_id;
-				$url = admin_url(
-					apply_filters( 'simple_history/admin_location', 'index' ) . '.php?page=simple_history_page'
-				);
-
-				// Each network site is added by WP core with id "blog-1", "blog-2" ... "blog-n"
-				// https://codex.wordpress.org/Function_Reference/add_node
-				$args = array(
-					'id' => $menu_id,
-					'parent' => $parent_menu_id,
-					'title' => _x( 'View History', 'Admin bar network name', 'simple-history' ),
-					'href' => $url,
-					'meta' => array(
-						'class' => 'ab-item--simplehistory',
-					),
-				);
-
-				$wp_admin_bar->add_node( $args );
-			} // End if().
-
-			restore_current_blog();
-		} // End foreach().
-	}
-
-	/**
-	 * Adds a "View history" item/shortcut to the admin bar
-	 *
-	 * Useful because Simple History is something at least the author of this plugin often use on a site :)
-	 *
-	 * @since 2.7.1
-	 */
-	public function add_admin_bar_menu_item( $wp_admin_bar ) {
-		/**
-		 * Filter to control if admin bar shortcut should be added
-		 *
-		 * @since 2.7.1
-		 *
-		 * @param bool $add_item Add item
-		 */
-		$add_item = apply_filters( 'simple_history/add_admin_bar_menu_item', true );
-
-		if ( ! $add_item ) {
-			return;
-		}
-
-		// Don't show for logged out users
-		if ( ! is_user_logged_in() ) {
-			return;
-		}
-
-		// Setting to show as page must be true
-		if ( ! $this->setting_show_as_page() ) {
-			return;
-		}
-
-		// User must have capability to view the history page
-		if ( ! current_user_can( $this->get_view_history_capability() ) ) {
-			return;
-		}
-
-		$menu_id = 'simple-history-view-history';
-		$parent_menu_id = 'site-name';
-		$url = admin_url( apply_filters( 'simple_history/admin_location', 'index' ) . '.php?page=simple_history_page' );
-
-		$args = array(
-			'id' => $menu_id,
-			'parent' => $parent_menu_id,
-			'title' => _x( 'View History', 'Admin bar name', 'simple-history' ),
-			'href' => $url,
-			'meta' => array(
-				'class' => 'ab-item--simplehistory',
-			),
-		);
-
-		$wp_admin_bar->add_node( $args );
 	}
 
 	/**
@@ -747,7 +623,9 @@ class Simple_History {
 	}
 
 	/**
-	 * Show a link to our settings page on the Plugins -> Installed Plugins screen
+	 * Add a link to the History Settings Page on the Plugins -> Installed Plugins screen.
+	 *
+	 * @param array $actions
 	 */
 	public function plugin_action_links( $actions, $b, $c, $d ) {
 		// Only add link if user has the right to view the settings page
@@ -755,7 +633,7 @@ class Simple_History {
 			return $actions;
 		}
 
-		$settings_page_url = menu_page_url( self::SETTINGS_MENU_SLUG, 0 );
+		$settings_page_url = menu_page_url( self::SETTINGS_MENU_SLUG, false );
 
 		if ( empty( $actions ) ) {
 			// Create array if actions is empty (and therefore is assumed to be a string by PHP & results in PHP 7.1+ fatal error due to trying to make array modifications on what's assumed to be a string)
@@ -764,6 +642,7 @@ class Simple_History {
 			// Convert the string (which it might've been retrieved as) to an array for future use as an array
 			$actions = [ $actions ];
 		}
+
 		$actions[] = "<a href='$settings_page_url'>" . __( 'Settings', 'simple-history' ) . '</a>';
 
 		return $actions;
---
+++ inc/services/class-network-menu-items.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Simple_History\Services;
+
+use Simple_History\Helpers;
+
+/**
+ * Class for core services to extend,
+ * i.e. services that are loaded early and are required for Simple History to work.
+ */
+class Network_Menu_Items extends Service {
+	public function loaded() {
+		add_action( 'admin_bar_menu', array( $this, 'add_admin_bar_network_menu_item' ), 40 );
+		add_action( 'admin_bar_menu', array( $this, 'add_admin_bar_menu_item' ), 40 );
+	}
+
+
+	/**
+	 * Adds a "View history" item/shortcut to the network admin, on blogs where Simple History is installed
+	 *
+	 * Useful because Simple History is something at least the author of this plugin often use on a site :)
+	 *
+	 * @since 2.7.1
+	 */
+	public function add_admin_bar_network_menu_item( $wp_admin_bar ) {
+		/**
+		 * Filter to control if admin bar shortcut should be added
+		 *
+		 * @since 2.7.1
+		 *
+		 * @param bool $add_item Add item
+		 */
+		$add_item = apply_filters( 'simple_history/add_admin_bar_network_menu_item', true );
+
+		if ( ! $add_item ) {
+			return;
+		}
+
+		// Don't show for logged out users or single site mode.
+		if ( ! is_user_logged_in() || ! is_multisite() ) {
+			return;
+		}
+
+		// Show only when the user has at least one site, or they're a super admin.
+		if ( ( is_countable( $wp_admin_bar->user->blogs ) ? count( $wp_admin_bar->user->blogs ) : 0 ) < 1 && ! is_super_admin() ) {
+			return;
+		}
+
+		// Setting to show as page must be true
+		if ( ! $this->simple_history->setting_show_as_page() ) {
+			return;
+		}
+
+		// User must have capability to view the history page
+		if ( ! current_user_can( $this->simple_history->get_view_history_capability() ) ) {
+			return;
+		}
+
+		foreach ( (array) $wp_admin_bar->user->blogs as $blog ) {
+			switch_to_blog( $blog->userblog_id );
+
+			// Check if simple History is active on this blog.
+			if ( Helpers::is_plugin_active( SIMPLE_HISTORY_BASENAME ) ) {
+				$menu_id = 'simple-history-blog-' . $blog->userblog_id;
+				$parent_menu_id = 'blog-' . $blog->userblog_id;
+				$url = admin_url(
+					apply_filters( 'simple_history/admin_location', 'index' ) . '.php?page=simple_history_page'
+				);
+
+				// Each network site is added by WP core with id "blog-1", "blog-2" ... "blog-n"
+				// https://codex.wordpress.org/Function_Reference/add_node
+				$args = array(
+					'id' => $menu_id,
+					'parent' => $parent_menu_id,
+					'title' => _x( 'View History', 'Admin bar network name', 'simple-history' ),
+					'href' => $url,
+					'meta' => array(
+						'class' => 'ab-item--simplehistory',
+					),
+				);
+
+				$wp_admin_bar->add_node( $args );
+			} // End if().
+
+			restore_current_blog();
+		} // End foreach().
+	}
+
+	/**
+	 * Adds a "View history" item/shortcut to the admin bar
+	 *
+	 * Useful because Simple History is something at least the author of this plugin often use on a site :)
+	 *
+	 * @since 2.7.1
+	 */
+	public function add_admin_bar_menu_item( $wp_admin_bar ) {
+		/**
+		 * Filter to control if admin bar shortcut should be added
+		 *
+		 * @since 2.7.1
+		 *
+		 * @param bool $add_item Add item
+		 */
+		$add_item = apply_filters( 'simple_history/add_admin_bar_menu_item', true );
+
+		if ( ! $add_item ) {
+			return;
+		}
+
+		// Don't show for logged out users
+		if ( ! is_user_logged_in() ) {
+			return;
+		}
+
+		// Setting to show as page must be true
+		if ( ! $this->simple_history->setting_show_as_page() ) {
+			return;
+		}
+
+		// User must have capability to view the history page
+		if ( ! current_user_can( $this->simple_history->get_view_history_capability() ) ) {
+			return;
+		}
+
+		$menu_id = 'simple-history-view-history';
+		$parent_menu_id = 'site-name';
+		$url = admin_url( apply_filters( 'simple_history/admin_location', 'index' ) . '.php?page=simple_history_page' );
+
+		$args = array(
+			'id' => $menu_id,
+			'parent' => $parent_menu_id,
+			'title' => _x( 'View History', 'Admin bar name', 'simple-history' ),
+			'href' => $url,
+			'meta' => array(
+				'class' => 'ab-item--simplehistory',
+			),
+		);
+
+		$wp_admin_bar->add_node( $args );
+	}
+
+}
