f6ef8c64d5f35b4ef83b48af39e546cebf23b36d
Pär Thernström <par.thernstrom@gmail.com>
2023-08-13 09:52:49 +0200

    Move dashboard widget to service

--- inc/class-simple-history.php
+++ inc/class-simple-history.php
@@ -116,6 +116,7 @@ class Simple_History {
 			Services\Setup_Pause_Resume_Actions::class,
 			Services\Setup_Purge_DB_Cron::class,
 			Services\API::class,
+			Services\Dashboard_Widget::class,
 		];
 	}
 
@@ -143,7 +144,6 @@ class Simple_History {
 	private function add_admin_actions() {
 		add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
 		add_action( 'admin_footer', array( $this, 'add_js_templates' ) );
-		add_action( 'wp_dashboard_setup', array( $this, 'add_dashboard_widget' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
 		add_action( 'admin_head', array( $this, 'on_admin_head' ) );
 		add_action( 'admin_footer', array( $this, 'on_admin_footer' ) );
@@ -767,49 +767,6 @@ class Simple_History {
 		$actions[] = "<a href='$settings_page_url'>" . __( 'Settings', 'simple-history' ) . '</a>';
 
 		return $actions;
-	}
-
-	/**
-	 * Maybe add a dashboard widget,
-	 * requires current user to have view history capability
-	 * and a setting to show dashboard to be set
-	 */
-	public function add_dashboard_widget() {
-		if ( $this->setting_show_on_dashboard() && current_user_can( $this->get_view_history_capability() ) ) {
-			/**
-			 * Filter to determine if history page should be added to page below dashboard or not
-			 *
-			 * @since 2.0.23
-			 *
-			 * @param bool $show_dashboard_widget Show the page or not
-			 */
-			$show_dashboard_widget = apply_filters( 'simple_history/show_dashboard_widget', true );
-
-			if ( $show_dashboard_widget ) {
-				wp_add_dashboard_widget(
-					'simple_history_dashboard_widget',
-					__( 'Simple History', 'simple-history' ),
-					array(
-						$this,
-						'dashboard_widget_output',
-					)
-				);
-			}
-		}
-	}
-
-	/**
-	 * Output html for the dashboard widget
-	 */
-	public function dashboard_widget_output() {
-		$pager_size = $this->get_pager_size_dashboard();
-
-		do_action( 'simple_history/dashboard/before_gui', $this );
-		?>
-		<div class="SimpleHistoryGui"
-			 data-pager-size='<?php echo esc_attr( $pager_size ); ?>'
-			 ></div>
-		<?php
 	}
 
 	/**
---
+++ inc/services/class-dashboard-widget.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Simple_History\Services;
+
+/**
+ * Class for core services to extend,
+ * i.e. services that are loaded early and are required for Simple History to work.
+ */
+class Dashboard_Widget extends Service {
+	public function loaded() {
+		add_action( 'wp_dashboard_setup', array( $this, 'add_dashboard_widget' ) );
+	}
+
+	/**
+	 * Add a dashboard widget.
+	 * Requires current user to have view history capability
+	 * and a setting to show dashboard to be set.
+	 */
+	public function add_dashboard_widget() {
+		if ( $this->simple_history->setting_show_on_dashboard() && current_user_can( $this->simple_history->get_view_history_capability() ) ) {
+			/**
+			 * Filter to determine if history page should be added to page below dashboard or not
+			 *
+			 * @since 2.0.23
+			 *
+			 * @param bool $show_dashboard_widget Show the page or not
+			 */
+			$show_dashboard_widget = apply_filters( 'simple_history/show_dashboard_widget', true );
+
+			if ( $show_dashboard_widget ) {
+				wp_add_dashboard_widget(
+					'simple_history_dashboard_widget',
+					__( 'Simple History', 'simple-history' ),
+					array(
+						$this,
+						'dashboard_widget_output',
+					)
+				);
+			}
+		}
+	}
+
+	/**
+	 * Output html for the dashboard widget
+	 */
+	public function dashboard_widget_output() {
+		$pager_size = $this->simple_history->get_pager_size_dashboard();
+
+		do_action( 'simple_history/dashboard/before_gui', $this );
+		?>
+		<div class="SimpleHistoryGui"
+			 data-pager-size='<?php echo esc_attr( $pager_size ); ?>'
+			 ></div>
+		<?php
+	}
+}
