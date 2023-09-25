641063fe6de60e39a5da2467d5759059839bdfac
Pär Thernström <par.thernstrom@gmail.com>
2023-09-25 11:22:54 +0200

    Admin header nav: show link to settings only if user has capability

--- inc/services/class-admin-pages.php
+++ inc/services/class-admin-pages.php
@@ -70,11 +70,15 @@ class Admin_Pages extends Service {
 				
 				<?php
 				// Add link to settings & tools.
+				if ( current_user_can( $this->simple_history->get_view_settings_capability() ) ) {
+					?>
+					<a href="<?php echo esc_url( menu_page_url( $this->simple_history::SETTINGS_MENU_SLUG, false ) ); ?>" class="sh-PageHeader-rightLink">
+						<span class="sh-PageHeader-settingsLinkIcon sh-Icon"></span>
+						<span class="sh-PageHeader-settingsLinkText"><?php esc_html_e( 'Settings & Tools', 'simple-history' ); ?></span>
+					</a>
+					<?php
+				}
 				?>
-				<a href="<?php echo esc_url( menu_page_url( $this->simple_history::SETTINGS_MENU_SLUG, false ) ); ?>" class="sh-PageHeader-rightLink">
-					<span class="sh-PageHeader-settingsLinkIcon sh-Icon sh-Icon--settings"></span>
-					<span class="sh-PageHeader-settingsLinkText"><?php esc_html_e( 'Settings & Tools', 'simple-history' ); ?></span>
-				</a>
 			</header>
 
 			<div class="wrap">
