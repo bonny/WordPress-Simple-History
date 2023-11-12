4e1c8bc52a007af4aa2ec4ea4f25b73a21b8f095
Pär Thernström <par.thernstrom@gmail.com>
2023-11-12 09:11:49 +0100

    Tests: Show db version

--- tests/wpunit/APHPVersionTest.php
+++ tests/wpunit/APHPVersionTest.php
@@ -5,6 +5,16 @@ class APHPVersionTest extends \Codeception\TestCase\WPTestCase {
 	// Based on solution:
 	// https://stackoverflow.com/questions/7493102/how-to-output-in-cli-during-execution-of-php-unit-tests
 	public function test_a_php_version() {
-		fwrite(STDERR, "\nLOG: phpversion(): " . phpversion() . "\n");
+		// Output PHP version.
+		fwrite(STDERR, "\nLOG: phpversion(): " . phpversion());
+
+		// Output MySQL/MariaDB version.
+		global $wpdb;
+		if ( empty( $wpdb->use_mysqli ) ) {
+			$mysqlVersion = mysql_get_server_info();
+		} else {
+			$mysqlVersion = mysqli_get_server_info( $wpdb->dbh );
+		}
+		fwrite(STDERR, "\nLOG: MySQL/MariaDB version: " . $mysqlVersion . "\n");
 	}
 }
