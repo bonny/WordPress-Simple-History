f155ceeb9794b93b42cdbe5fce152fc592ecb742
Pär Thernström <par.thernstrom@gmail.com>
2024-01-23 21:27:07 +0100

    Tests: Start working on test for post logger and adding/removing terms on post

--- tests/acceptance/SimplePostLoggerCest.php
+++ tests/acceptance/SimplePostLoggerCest.php
@@ -60,4 +60,47 @@ class SimplePostLoggerCest
             'post_new_post_content' => '<!-- wp:paragraph -->' . PHP_EOL . '<p>This is text in paragraph.</p>' . PHP_EOL . '<!-- /wp:paragraph -->'
         ]);
     }
+
+    /**
+     * Create a blog post and then add tags and categories to it
+     * and confirm that the log messages are correct.
+     *
+     * @param Admin $I
+     */
+    public function testPostCategoriesAddedAndRemoved(Admin $I) {
+        $I->amOnAdminPage('edit.php?post_type=post');
+        $I->click('Add New', '.wrap');
+
+        // Close Welcome guide.
+        $I->click('.edit-post-welcome-guide button.components-button');
+
+        // Edit post and save as draft.
+        $I->click('.wp-block-post-title');
+        $I->pressKey('.wp-block-post-title', ['H', 'e', 'l', 'l', 'o']);
+        $I->click('Save draft');
+        $I->waitForText('Draft saved');
+        $I->seeLogMessage('Created post "Hello"');
+        $I->seeLogContext([
+            'post_type' => 'post',
+            'post_title' => 'Hello',
+            '_message_key' => 'post_created',
+        ]);
+
+        /**
+         *  Add tags and categories.
+         */
+
+         // Todo: this part.
+         // Can't get the tags and categories panel to open.
+        
+        // Expand categories panel.
+        // $I->click('Categories', 'button span');
+        $I->click('.components-button components-panel__body-toggle:nth-child(1)');
+        // $I->findElement('.editor-post-taxonomies__hierarchical-terms-add')->click();
+
+        // Scroll down to "Add New Category" button.
+        // $I->scrollTo('.editor-post-taxonomies__hierarchical-terms-add');
+        
+        $I->makeScreenshot('post-created');
+    }
 }
