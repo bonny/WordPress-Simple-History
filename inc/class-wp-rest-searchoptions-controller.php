c3d4b7da680aa43acee72a27a505e566ce49ec87
Pär Thernström <par.thernstrom@gmail.com>
2024-07-11 15:52:18 +0200

    Add rest endpoint for searching users

--- inc/class-wp-rest-searchoptions-controller.php
+++ inc/class-wp-rest-searchoptions-controller.php
@@ -42,6 +42,43 @@ class WP_REST_SearchOptions_Controller extends WP_REST_Controller {
 				],
 			],
 		);
+
+		// GET /wp-json/simple-history/v1/search-user.
+		register_rest_route(
+			$this->namespace,
+			'/search-user',
+			[
+				[
+					'methods'             => WP_REST_Server::READABLE,
+					'callback'            => [ $this, 'get_items_for_search_user' ],
+					'permission_callback' => [ $this, 'get_items_permissions_check_for_search_user' ],
+				],
+			],
+		);
+	}
+
+	/**
+	 * Get items for search user.
+	 *
+	 * @param WP_REST_Request $request Request object.
+	 * @return WP_REST_Response Response object.
+	 */
+	public function get_items_for_search_user( $request ) {
+		$data = [];
+
+		$q = trim( sanitize_text_field( wp_unslash( $_GET['q'] ?? '' ) ) );
+
+		if ( empty( $q ) ) {
+			return new WP_Error(
+				'rest_invalid_param',
+				__( 'Please provide a search query.', 'simple-history' ),
+				[ 'status' => 400 ]
+			);
+		}
+
+		$data = $this->search_user( $q );
+
+		return rest_ensure_response( $data );
 	}
 
 	/**
@@ -66,8 +103,6 @@ class WP_REST_SearchOptions_Controller extends WP_REST_Controller {
 	/**
 	 * Checks if a given request has access to read posts.
 	 *
-	 * @since 4.7.0
-	 *
 	 * @param WP_REST_Request $request Full details about the request.
 	 * @return true|WP_Error True if the request has read access, WP_Error object otherwise.
 	 */
@@ -85,21 +120,46 @@ class WP_REST_SearchOptions_Controller extends WP_REST_Controller {
 	}
 
 	/**
+	 * Checks if a given request has access to read posts.
+	 *
+	 * @param WP_REST_Request $request Full details about the request.
+	 * @return true|WP_Error True if the request has read access, WP_Error object otherwise.
+	 */
+	public function get_items_permissions_check_for_search_user( $request ) {
+		// User must be logged in.
+		if ( ! is_user_logged_in() ) {
+			return new WP_Error(
+				'rest_forbidden_context',
+				__( 'Sorry, you are not allowed to search users.', 'simple-history' ),
+				[ 'status' => rest_authorization_required_code() ]
+			);
+		}
+
+		// user must have list_users capability (default super admin + administrators have this).
+		if ( ! current_user_can( 'list_users' ) ) {
+			return new WP_Error(
+				'rest_forbidden_context',
+				__( 'Sorry, you are not allowed to search users.', 'simple-history' ),
+				[ 'status' => rest_authorization_required_code() ]
+			);
+		}
+
+		return true;
+	}
+
+	/**
 	 * Get items.
 	 *
 	 * @param WP_REST_Request $request Request object.
 	 * @return WP_REST_Response Response object.
 	 */
 	public function get_items( $request ) {
-		$data = [];
+		$data = [
+			'dates' => Helpers::get_data_for_date_filter(),
+			'loggers' => $this->get_loggers_and_messages(),
+		];
 
-		$data['dates'] = Helpers::get_data_for_date_filter();
-
-		$data['loggers'] = $this->get_loggers_and_messages();
-
-		$response = rest_ensure_response( $data );
-
-		return $response;
+		return rest_ensure_response( $data );
 	}
 
 	/**
@@ -135,7 +195,6 @@ class WP_REST_SearchOptions_Controller extends WP_REST_Controller {
 						$arr_all_search_messages[ $key ] = $logger_slug . ':' . $val;
 					}
 
-					// printf( '<option value="%2$s">%1$s</option>', esc_attr( $logger_info['labels']['search']['label_all'] ), esc_attr( implode( ',', $arr_all_search_messages ) ) );
 					$logger_search_data['search_all'] = [
 						'label' => $logger_info['labels']['search']['label_all'],
 						'options' => $arr_all_search_messages,
@@ -148,12 +207,6 @@ class WP_REST_SearchOptions_Controller extends WP_REST_Controller {
 						$option_messages[ $key ] = $logger_slug . ':' . $val;
 					}
 
-					// $str_option_messages = implode( ',', $option_messages );
-					// printf(
-					// '<option value="%2$s">%1$s</option>',
-					// esc_attr( $option_key ), // 1
-					// esc_attr( $str_option_messages ) // 2
-					// );
 					$logger_search_data['search_options'][] = [
 						'label' => $option_key,
 						'options' => $option_messages,
@@ -169,5 +222,26 @@ class WP_REST_SearchOptions_Controller extends WP_REST_Controller {
 		}
 
 		return $loggers_and_messages;
+	}
+
+	/**
+	 * Search for users.
+	 *
+	 * @param string $q Search query.
+	 * @return array List of users.
+	 */
+	public function search_user( $q ) {
+		// Search both current users and all logged rows,
+		// because a user can change email
+		// search in context: user_id, user_email, user_login
+		// search in wp_users: login, nicename, user_email
+		// search and get users. make sure to use "fields" and "number" or we can get timeout/use lots of memory if we have a large amount of users.
+		return get_users(
+			[
+				'search' => "*{$q}*",
+				'fields' => array( 'ID', 'user_login', 'user_nicename', 'user_email', 'display_name' ),
+				'number' => 20,
+			]
+		);
 	}
 }
