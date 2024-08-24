4512d148f5be8b68148c2d48ab30b7d98e5fb76c
Pär Thernström <par.thernstrom@gmail.com>
2024-08-24 18:04:04 +0200

    Fix guicest

--- tests/acceptance/GUICest.php
+++ tests/acceptance/GUICest.php
@@ -10,23 +10,25 @@ class GUICest {
                 $I->amOnAdminPage( 'index.php?page=simple_history_page' );
 
                 $I->see( 'Simple History' );
+               
+                $I->waitForElement( '.SimpleHistoryLogitem__text' );
                 $I->see('Logged in', '.SimpleHistoryLogitem__text');
 
                 // Search filters, unexpanded and expanded.
-                $I->dontSee('Log levels:');
-                $I->dontSee('Message types:');
-                $I->dontSee('users:');
+                $I->dontSee('Log levels');
+                $I->dontSee('Message types');
+                $I->dontSee('Enter 2 or more characters to search for users.');
 
                 $I->click('Show search options');
-                $I->see('Log levels:');
-                $I->see('Message types:');
-                $I->see('Users:');
+                $I->see('Log levels');
+                $I->see('Message types');
+                $I->see('Users');
 
                 // Sidebar boxes.
                 $I->see('Stats');
                 $I->see('Number of events per day.');
                 $I->see('Settings');
-                $I->see('Visit the settings page');
+                
                 $I->see('Review this plugin if you like it');
                 $I->see('Visit the support forum');
                 $I->see('Donate to support development');
