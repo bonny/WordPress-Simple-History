54b38048819680f2ba2a7012d390c95b1a9dd238
Pär Thernström <par.thernstrom@gmail.com>
2024-08-25 11:46:38 +0200

    Fix wpcli test

--- tests/functional/WPCliCest.php
+++ tests/functional/WPCliCest.php
@@ -5,7 +5,7 @@ class WPCliCest {
 	public function test_wp_cli_commands( FunctionalTester $I ) {
         // Test WP Version so we are not surprised by WP version changes.
         $I->cli('--allow-root core version');
-        $I->seeInShellOutput('6.1.1');
+        $I->seeInShellOutput('6.6.1');
 
         $I->cli('--allow-root simple-history');
         $I->seeInShellOutput('usage: wp simple-history list [--format=<format>] [--count=<count>]');
