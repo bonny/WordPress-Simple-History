ad9539e34402542d356d6ce8578683af8402a5c3
Pär Thernström <par.thernstrom@gmail.com>
2024-10-24 09:08:55 +0200

    Build

--- build/index-admin-bar.asset.php
+++ build/index-admin-bar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'feb53efa184f3cbafa9a');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '29c14b6e4041e6d95b7a');
