ee11864940be61577bb9821e686c7193feb9bab3
Pär Thernström <par.thernstrom@gmail.com>
2024-12-16 07:37:53 +0100

    Remove old sidebar postbox

--- dropins/class-sidebar-add-ons-dropin.php
+++ dropins/class-sidebar-add-ons-dropin.php
@@ -12,7 +12,6 @@ class Sidebar_Add_Ons_Dropin extends Dropin {
 	 * Add actions when dropin is loaded.
 	 */
 	public function loaded() {
-		// add_action( 'simple_history/dropin/sidebar/sidebar_html', [ $this, 'on_sidebar_html_general_message' ], 5 );
 		add_action( 'simple_history/dropin/sidebar/sidebar_html', [ $this, 'on_sidebar_html_woocommerce' ], 5 );
 		add_action( 'simple_history/dropin/sidebar/sidebar_html', [ $this, 'on_sidebar_html_premium_promo' ], 5 );
 	}
@@ -102,54 +101,6 @@ class Sidebar_Add_Ons_Dropin extends Dropin {
 				<p>
 					<a href="<?php echo esc_url( $woocommerce_logger_url ); ?>" class="sh-ExternalLink" target="_blank">
 						<?php esc_html_e( 'View WooCommerce add-on.', 'simple-history' ); ?>
-					</a>
-				</p>
-			</div>
-		</div>
-		<?php
-	}
-
-	/**
-	 * Output HTML for a general add-ons box in the sidebar.
-	 */
-	public function on_sidebar_html_general_message() {
-		?>
-		<div class="postbox">
-
-			<h3 class="hndle">
-				<?php esc_html_e( 'Add-ons', 'simple-history' ); ?>
-				<em class="sh-PageHeader-settingsLinkIcon-new"><?php esc_html_e( 'New', 'simple-history' ); ?></em>
-			</h3>
-
-			<div class="inside">
-				<p>
-					<?php esc_html_e( 'Now you can enhance and extend Simple History with add-ons.', 'simple-history' ); ?>
-				</p>
-			
-				<?php
-				/*
-				<ul>
-					<li>
-						<strong><?php esc_html_e( 'Premium:', 'simple-history' ); ?></strong>
-						<?php esc_html_e( 'Control number of days to keep the log, limit number of login attempts logged, store full IP-addresses, and a bonus JSON feed!', 'simple-history' ); ?>
-					</li>
-
-					<li>
-						<strong><?php esc_html_e( 'WooCommerce Logger:', 'simple-history' ); ?></strong>
-						<?php esc_html_e( 'Detailed WooCommerce activity logs. (Coming soon.)', 'simple-history' ); ?>
-					</li>
-
-					<li>
-						<strong><?php esc_html_e( 'Developer Tools:', 'simple-history' ); ?></strong>
-						<?php esc_html_e( 'Email, HTTP request, wp-cron job monitoring. (Coming soon.)', 'simple-history' ); ?>
-					</li>
-				</ul>
-				*/
-				?>
-
-				<p>
-					<a href="https://simple-history.com/add-ons/?utm_source=wpadmin&utm_content=addons-sidebar" class="sh-ExternalLink" target="_blank">
-						<?php esc_html_e( 'View add-ons.', 'simple-history' ); ?>
 					</a>
 				</p>
 			</div>
