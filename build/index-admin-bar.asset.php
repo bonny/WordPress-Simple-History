82bb8afd042ae3e66ad0e220b74dc137f6ad1fb3
Pär Thernström <par.thernstrom@gmail.com>
2024-12-30 13:59:43 +0100

    Ignore build folder

--- build/index-admin-bar.asset.php
+++ build/index-admin-bar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'fcb5f2850428077485ef');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '51ad0db61ef51e5b257f');
