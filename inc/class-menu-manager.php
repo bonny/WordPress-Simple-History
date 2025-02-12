e63301e4d4c09ee199da295e87c6b5fd9f595591
Pär Thernström <par.thernstrom@gmail.com>
2025-02-12 22:03:50 +0100

    First iteration of new Menu_Manager and Menu_Page classes

---
+++ inc/class-menu-manager.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace Simple_History;
+
+/**
+ * Menu manager class that handles registration and organization of admin menu pages.
+ */
+class Menu_Manager {
+	/** @var array<string,Menu_Page> Array of all registered menu pages. */
+	private $pages = [];
+
+	/**
+	 * Add a page to be managed.
+	 *
+	 * @param Menu_Page $page Page object to add.
+	 * @return self
+	 */
+	public function add_page( Menu_Page $page ) {
+		$this->pages[ $page->get_menu_slug() ] = $page;
+		return $this;
+	}
+
+	/**
+	 * Get a page by its slug.
+	 *
+	 * @param string $slug Page slug.
+	 * @return Menu_Page|null Menu page if found, null if not found.
+	 */
+	public function get_page( $slug ) {
+		return $this->pages[ $slug ] ?? null;
+	}
+
+	/**
+	 * Get all registered pages.
+	 *
+	 * @return array<string,Menu_Page>
+	 */
+	public function get_pages() {
+		return $this->pages;
+	}
+
+	/**
+	 * Register all menu pages with WordPress.
+	 * Called during admin_menu.
+	 */
+	public function register_pages() {
+		foreach ( $this->pages as $page ) {
+			$location = $page->get_location();
+
+			switch ( $location ) {
+				case 'menu_top':
+					$this->add_top_level_menu_page( $page );
+					break;
+				case 'menu_bottom':
+					$this->add_top_level_menu_page( $page, 'bottom' );
+					break;
+				case 'dashboard':
+					$this->add_dashboard_page( $page );
+					break;
+				case 'settings':
+					$this->add_settings_page( $page );
+					break;
+				case 'tools':
+					$this->add_tools_page( $page );
+					break;
+				default:
+					// Handle sub-pages that have parent set but no explicit location.
+					if ( $page->get_parent() ) {
+						$this->add_submenu_page( $page );
+					}
+			}
+		}
+	}
+
+	/**
+	 * Add a top level menu page.
+	 *
+	 * @param Menu_Page $page Page to add.
+	 * @param string    $position 'top' or 'bottom', determines menu position.
+	 */
+	private function add_top_level_menu_page( Menu_Page $page, $position = 'top' ) {
+		$menu_position = $position === 'bottom' ? 100 : 3;
+
+		$hookname = add_menu_page(
+			$page->get_title(),
+			$page->get_menu_title(),
+			$page->get_capability(),
+			$page->get_menu_slug(),
+			[ $page, 'render' ],
+			$page->get_icon(),
+			$menu_position
+		);
+
+		$page->set_hook_name( $hookname );
+
+		// Add any child pages.
+		foreach ( $this->get_child_pages( $page ) as $child_page ) {
+			$this->add_submenu_page( $child_page );
+		}
+	}
+
+	/**
+	 * Add a dashboard page.
+	 *
+	 * @param Menu_Page $page Page to add.
+	 */
+	private function add_dashboard_page( Menu_Page $page ) {
+		$hookname = add_dashboard_page(
+			$page->get_title(),
+			$page->get_menu_title(),
+			$page->get_capability(),
+			$page->get_menu_slug(),
+			[ $page, 'render' ]
+		);
+
+		$page->set_hook_name( $hookname );
+	}
+
+	/**
+	 * Add a settings/options page.
+	 *
+	 * @param Menu_Page $page Page to add.
+	 */
+	private function add_settings_page( Menu_Page $page ) {
+		$hookname = add_options_page(
+			$page->get_title(),
+			$page->get_menu_title(),
+			$page->get_capability(),
+			$page->get_menu_slug(),
+			[ $page, 'render' ]
+		);
+
+		$page->set_hook_name( $hookname );
+	}
+
+	/**
+	 * Add a tools page.
+	 *
+	 * @param Menu_Page $page Page to add.
+	 */
+	private function add_tools_page( Menu_Page $page ) {
+		$hookname = add_management_page(
+			$page->get_title(),
+			$page->get_menu_title(),
+			$page->get_capability(),
+			$page->get_menu_slug(),
+			[ $page, 'render' ]
+		);
+
+		$page->set_hook_name( $hookname );
+	}
+
+	/**
+	 * Add a submenu page.
+	 *
+	 * @param Menu_Page $page Page to add.
+	 */
+	private function add_submenu_page( Menu_Page $page ) {
+		$parent = $page->get_parent();
+
+		if ( ! $parent ) {
+			return;
+		}
+
+		$hookname = add_submenu_page(
+			$parent->get_menu_slug(),
+			$page->get_title(),
+			$page->get_menu_title(),
+			$page->get_capability(),
+			$page->get_menu_slug(),
+			[ $page, 'render' ]
+		);
+
+		$page->set_hook_name( $hookname );
+	}
+
+	/**
+	 * Get all child pages of a parent page.
+	 *
+	 * @param Menu_Page $parent_page Parent page to get children for.
+	 * @return array<Menu_Page>
+	 */
+	private function get_child_pages( Menu_Page $parent_page ) {
+		$children = [];
+
+		foreach ( $this->pages as $page ) {
+			if ( $page->get_parent() === $parent_page ) {
+				$children[] = $page;
+			}
+		}
+
+		// Sort children by order.
+		usort(
+			$children,
+			function ( $a, $b ) {
+				return $a->get_order() - $b->get_order();
+			}
+		);
+
+		return $children;
+	}
+}
---
+++ inc/class-menu-page.php
@@ -0,0 +1,238 @@
+<?php
+
+namespace Simple_History;
+
+/**
+ * Class that represents a menu page in the WordPress admin.
+ */
+class Menu_Page {
+	/** @var string Page title shown in browser title. */
+	private $title = '';
+
+	/** @var string Menu title shown in admin menu. */
+	private $menu_title = '';
+
+	/** @var string Required capability to view page. */
+	private $capability = 'manage_options';
+
+	/** @var string Unique menu slug. */
+	private $menu_slug = '';
+
+	/** @var callable|string|array Callback to render page contents. */
+	private $callback;
+
+	/** @var string Dashicon name or URL to icon. */
+	private $icon = '';
+
+	/** @var int Order among sibling menu items. */
+	private $order = 10;
+
+	/** @var Menu_Page|null Parent page if this is a submenu item. */
+	private $parent = null;
+
+	/** @var string Location in admin menu. One of 'menu_top', 'menu_bottom', 'dashboard', 'settings', 'tools'. */
+	private $location = 'menu_top';
+
+	/** @var string Hook name/page ID returned by add_menu_page() etc. */
+	private $hook_name = '';
+
+	/**
+	 * Set the page title.
+	 *
+	 * @param string $title Page title.
+	 * @return self
+	 */
+	public function title( $title ) {
+		$this->title = $title;
+		return $this;
+	}
+
+	/**
+	 * Set the menu title.
+	 *
+	 * @param string $menu_title Menu title.
+	 * @return self
+	 */
+	public function menu_title( $menu_title ) {
+		$this->menu_title = $menu_title;
+		return $this;
+	}
+
+	/**
+	 * Set required capability.
+	 *
+	 * @param string $capability Required capability.
+	 * @return self
+	 */
+	public function capability( $capability ) {
+		$this->capability = $capability;
+		return $this;
+	}
+
+	/**
+	 * Set menu slug.
+	 *
+	 * @param string $menu_slug Menu slug.
+	 * @return self
+	 */
+	public function menu_slug( $menu_slug ) {
+		$this->menu_slug = $menu_slug;
+		return $this;
+	}
+
+	/**
+	 * Set render callback.
+	 *
+	 * @param callable|string|array $callback Callback function/method.
+	 * @return self
+	 */
+	public function callback( $callback ) {
+		$this->callback = $callback;
+		return $this;
+	}
+
+	/**
+	 * Set menu icon.
+	 *
+	 * @param string $icon Icon name or URL.
+	 * @return self
+	 */
+	public function icon( $icon ) {
+		$this->icon = $icon;
+		return $this;
+	}
+
+	/**
+	 * Set menu order.
+	 *
+	 * @param int $order Order number.
+	 * @return self
+	 */
+	public function order( $order ) {
+		$this->order = $order;
+		return $this;
+	}
+
+	/**
+	 * Set parent page.
+	 *
+	 * @param Menu_Page $parent Parent page.
+	 * @return self
+	 */
+	public function parent( Menu_Page $parent ) {
+		$this->parent = $parent;
+		return $this;
+	}
+
+	/**
+	 * Set menu location.
+	 *
+	 * @param string $location Location in admin menu.
+	 * @return self
+	 */
+	public function location( $location ) {
+		$this->location = $location;
+		return $this;
+	}
+
+	/**
+	 * Get page title.
+	 *
+	 * @return string
+	 */
+	public function get_title() {
+		return $this->title;
+	}
+
+	/**
+	 * Get menu title.
+	 *
+	 * @return string
+	 */
+	public function get_menu_title() {
+		return $this->menu_title;
+	}
+
+	/**
+	 * Get required capability.
+	 *
+	 * @return string
+	 */
+	public function get_capability() {
+		return $this->capability;
+	}
+
+	/**
+	 * Get menu slug.
+	 *
+	 * @return string
+	 */
+	public function get_menu_slug() {
+		return $this->menu_slug;
+	}
+
+	/**
+	 * Get icon.
+	 *
+	 * @return string
+	 */
+	public function get_icon() {
+		return $this->icon;
+	}
+
+	/**
+	 * Get order.
+	 *
+	 * @return int
+	 */
+	public function get_order() {
+		return $this->order;
+	}
+
+	/**
+	 * Get parent page.
+	 *
+	 * @return Menu_Page|null
+	 */
+	public function get_parent() {
+		return $this->parent;
+	}
+
+	/**
+	 * Get menu location.
+	 *
+	 * @return string
+	 */
+	public function get_location() {
+		return $this->location;
+	}
+
+	/**
+	 * Set the hook name returned by add_menu_page() etc.
+	 *
+	 * @param string $hook_name Hook name.
+	 */
+	public function set_hook_name( $hook_name ) {
+		$this->hook_name = $hook_name;
+	}
+
+	/**
+	 * Get the hook name for this page.
+	 *
+	 * @return string
+	 */
+	public function get_hook_name() {
+		return $this->hook_name;
+	}
+
+	/**
+	 * Render the page contents.
+	 */
+	public function render() {
+		if ( is_callable( $this->callback ) ) {
+			call_user_func( $this->callback );
+		} elseif ( is_string( $this->callback ) ) {
+			echo wp_kses_post( $this->callback );
+		}
+	}
+}
