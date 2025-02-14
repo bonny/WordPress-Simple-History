72dc5565e8056eebac05c24e5e2af885342a602b
Pär Thernström <par.thernstrom@gmail.com>
2025-02-14 13:46:02 +0100

    Don't register pages in menu service

--- inc/services/class-menu-service.php
+++ inc/services/class-menu-service.php
@@ -29,44 +29,11 @@ class Menu_Service extends Service {
 	 */
 	public function loaded() {
 		// Register the pages early.
-		add_action( 'init', [ $this, 'register_pages' ], 10 );
+		// add_action( 'init', [ $this, 'register_pages' ], 10 );
+		// Services handle their page registration where appropriate.
 
 		// Register menus late in admin_menu so other plugins can modify their menus first.
 		add_action( 'admin_menu', [ $this, 'register_admin_menus' ], 100 );
-	}
-
-	/**
-	 * Register core menu pages.
-	 */
-	public function register_pages() {
-		// Main log page.
-		$main_log_page = ( new \Simple_History\Menu_Page() )
-				->set_menu_slug( 'simple-history-main' )
-				->set_title( __( 'Simple History', 'simple-history' ) )
-				->set_menu_title( __( 'Simple History', 'simple-history' ) )
-				->set_capability( Helpers::get_view_history_capability() )
-				->set_icon( 'dashicons-clock' )
-				->set_callback( [ $this->simple_history, 'history_page_output' ] );
-
-		// Location based on user preference.
-		if ( Helpers::setting_show_as_page() ) {
-			$main_log_page->set_location( 'dashboard' );
-		} else {
-			$main_log_page->set_location( 'menu_top' );
-		}
-
-		$this->menu_manager->add_page( $main_log_page );
-
-		// Settings page.
-		$settings_page = ( new \Simple_History\Menu_Page() )
-				->set_menu_slug( 'simple-history-settings' )
-				->set_title( __( 'Simple History Settings', 'simple-history' ) )
-				->set_menu_title( __( 'Settings', 'simple-history' ) )
-				->set_capability( Helpers::get_view_settings_capability() )
-				->set_callback( [ $this->simple_history, 'settings_page_output' ] )
-				->set_parent( $main_log_page );
-
-		$this->menu_manager->add_page( $settings_page );
 	}
 
 	/**
