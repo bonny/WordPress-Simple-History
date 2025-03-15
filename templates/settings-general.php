1d52ec2d2f487c07cbde78caf5c78e41c0e29c38
Pär Thernström <par.thernstrom@gmail.com>
2025-03-15 09:02:27 +0100

    Refactor general settings page layout and add comments regarding output behavior due to a known bug in do_settings_sections().

--- templates/settings-general.php
+++ templates/settings-general.php
@@ -4,18 +4,35 @@ namespace Simple_History;
 use Simple_History\Simple_History;
 
 defined( 'ABSPATH' ) || die();
+
+/**
+ * This is the output of the general settings page.
+ */
 ?>
-
 <div class="wrap sh-Page-content">
-	<form method="post" action="options.php">
-		<?php
-		// Prints out all settings sections added to a particular settings page.
-		do_settings_sections( Simple_History::SETTINGS_MENU_SLUG );
+	<div class="sh-grid sh-grid-cols-1/2">
+		<form method="post" action="options.php">
+			<?php
+			// Prints out all settings sections added to a particular settings page.
+			do_settings_sections( Simple_History::SETTINGS_MENU_SLUG );
 
-		// Output nonce, action, and option_page fields.
-		settings_fields( Simple_History::SETTINGS_GENERAL_OPTION_GROUP );
+			// Output nonce, action, and option_page fields.
+			settings_fields( Simple_History::SETTINGS_GENERAL_OPTION_GROUP );
 
-		submit_button();
-		?>
-	</form>
+			submit_button();
+			?>
+		</form>
+
+		<!-- 
+		This is currently placed placed  wrong due to bug in do_settings_sections() that is
+		causing it to not output after_section-html.
+		Related track tickets:
+		https://core.trac.wordpress.org/ticket/62746
+		https://core.trac.wordpress.org/changeset/59564
+		-->
+		<div>
+			
+		</div>
+
+	</div>
 </div>
