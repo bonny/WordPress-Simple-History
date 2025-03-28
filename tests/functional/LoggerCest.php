0ab30013cb53d72329b660a19e8f4e53f96fbe8e
Pär Thernström <par.thernstrom@gmail.com>
2025-03-28 15:31:08 +0100

    Update LoggerCest to navigate to the correct help and support debug tab in admin page tests

--- tests/functional/LoggerCest.php
+++ tests/functional/LoggerCest.php
@@ -3,9 +3,9 @@
 class LoggerCest {
 	public function test_can_get_logger_output( FunctionalTester $I ) {
         $I->loginAsAdmin();
-        $I->amOnAdminPage('admin.php?page=simple_history_debug');
+        $I->amOnAdminPage('admin.php?page=simple_history_help_support&selected-tab=simple_history_help_support_general&selected-sub-tab=simple_history_help_support_debug');
         $I->canSee('Tests logger');
         $I->canSee('Output in footer from the logger with slug tests');
     }
 }
-//selected-sub-tab
+
