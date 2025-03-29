cc5df4ae28df89c5e425daee143cbb33c4b340fa
Pär Thernström <par.thernstrom@gmail.com>
2025-03-29 08:56:43 +0100

    Add compat functions esc_xml() and rest_is_field_included()

--- inc/class-compat.php
+++ inc/class-compat.php
@@ -2,9 +2,6 @@
 
 namespace Simple_History;
 
-use Simple_History\Simple_History;
-use Simple_History\Services\Setup_Settings_Page;
-
 /**
  * Compatibility functions,
  * so we can use new functions in older versions of WordPress.
@@ -27,8 +24,6 @@ class Compat {
 	 *
 	 * Note that, unlike `date_i18n()`, this function accepts a true Unix timestamp, not summed
 	 * with timezone offset .
-	 *
-	 * @since 5.3.0
 	 *
 	 * @global WP_Locale $wp_locale WordPress date and time locale object .
 	 *
@@ -119,8 +114,120 @@ class Compat {
 		 * @param int          $timestamp Unix timestamp.
 		 * @param DateTimeZone $timezone  Timezone.
 		 */
+		// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
 		$date = apply_filters( 'wp_date', $date, $format, $timestamp, $timezone );
 
 		return $date;
+	}
+
+	/**
+	 * Escaping for XML blocks.
+	 *
+	 * @since 5.5.0
+	 *
+	 * @param string $text Text to escape.
+	 * @return string Escaped text.
+	 */
+	public static function esc_xml( $text ) {
+		// Use esc_xml() that was added in WordPress 5.5.0, if available.
+		if ( function_exists( 'esc_xml' ) ) {
+			return esc_xml( $text );
+		}
+
+		// Start of function esc_xml() as it was in WordPress 6.7.
+		$safe_text = wp_check_invalid_utf8( $text );
+
+		$cdata_regex = '\<\!\[CDATA\[.*?\]\]\>';
+		$regex       = <<<EOF
+/
+	(?=.*?{$cdata_regex})                 # lookahead that will match anything followed by a CDATA Section
+	(?<non_cdata_followed_by_cdata>(.*?)) # the "anything" matched by the lookahead
+	(?<cdata>({$cdata_regex}))            # the CDATA Section matched by the lookahead
+
+|	                                      # alternative
+
+	(?<non_cdata>(.*))                    # non-CDATA Section
+/sx
+EOF;
+
+		$safe_text = (string) preg_replace_callback(
+			$regex,
+			static function ( $matches ) {
+				if ( ! isset( $matches[0] ) ) {
+					return '';
+				}
+
+				if ( isset( $matches['non_cdata'] ) ) {
+					// escape HTML entities in the non-CDATA Section.
+					return _wp_specialchars( $matches['non_cdata'], ENT_XML1 );
+				}
+
+				// Return the CDATA Section unchanged, escape HTML entities in the rest.
+				return _wp_specialchars( $matches['non_cdata_followed_by_cdata'], ENT_XML1 ) . $matches['cdata'];
+			},
+			$safe_text
+		);
+
+		/**
+		 * Filters a string cleaned and escaped for output in XML.
+		 *
+		 * Text passed to esc_xml() is stripped of invalid or special characters
+		 * before output. HTML named character references are converted to their
+		 * equivalent code points.
+		 *
+		 * @since 5.5.0
+		 *
+		 * @param string $safe_text The text after it has been escaped.
+		 * @param string $text      The text prior to being escaped.
+		 */
+		// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
+		return apply_filters( 'esc_xml', $safe_text, $text );
+	}
+
+
+	/**
+	 * Given an array of fields to include in a response, some of which may be
+	 * `nested.fields`, determine whether the provided field should be included
+	 * in the response body.
+	 *
+	 * If a parent field is passed in, the presence of any nested field within
+	 * that parent will cause the method to return `true`. For example "title"
+	 * will return true if any of `title`, `title.raw` or `title.rendered` is
+	 * provided.
+	 *
+	 * @param string $field  A field to test for inclusion in the response body.
+	 * @param array  $fields An array of string fields supported by the endpoint.
+	 * @return bool Whether to include the field or not.
+	 */
+	public static function rest_is_field_included( $field, $fields ) {
+		// Use rest_is_field_included() that was added in WordPress 5.3.0, if available.
+		if ( function_exists( 'rest_is_field_included' ) ) {
+			return rest_is_field_included( $field, $fields );
+		}
+
+		// Start of function rest_is_field_included() as it was in WordPress 6.7.
+		if ( in_array( $field, $fields, true ) ) {
+			return true;
+		}
+
+		foreach ( $fields as $accepted_field ) {
+			/*
+			 * Check to see if $field is the parent of any item in $fields.
+			 * A field "parent" should be accepted if "parent.child" is accepted.
+			 */
+			if ( str_starts_with( $accepted_field, "$field." ) ) {
+				return true;
+			}
+
+			/*
+			 * Conversely, if "parent" is accepted, all "parent.child" fields
+			 * should also be accepted.
+			 */
+			if ( str_starts_with( $field, "$accepted_field." ) ) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
