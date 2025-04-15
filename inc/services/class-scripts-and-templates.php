eefe09c063d2fc7d013c84f8b1e0db6d9a38b6dd
Pär Thernström <par.thernstrom@gmail.com>
2025-04-15 16:50:10 +0200

    Use early return when enqueueing scripts and css

--- inc/services/class-scripts-and-templates.php
+++ inc/services/class-scripts-and-templates.php
@@ -37,54 +37,58 @@ class Scripts_And_Templates extends Service {
 	 * @param string $hook The current admin page.
 	 */
 	public function enqueue_admin_scripts( $hook ) {
-		if ( Helpers::is_on_our_own_pages() ) {
-			add_thickbox();
-
-			wp_enqueue_style(
-				'simple_history_styles',
-				SIMPLE_HISTORY_DIR_URL . 'css/styles.css',
-				false,
-				SIMPLE_HISTORY_VERSION
-			);
-
-			wp_enqueue_style(
-				'simple_history_utility_styles',
-				SIMPLE_HISTORY_DIR_URL . 'css/utility-classes.css',
-				false,
-				SIMPLE_HISTORY_VERSION
-			);
-
-			wp_enqueue_script(
-				'simple_history_script',
-				SIMPLE_HISTORY_DIR_URL . 'js/scripts.js',
-				array( 'jquery', 'backbone', 'wp-util' ),
-				SIMPLE_HISTORY_VERSION,
-				true
-			);
-
-			// Translations that we use in JavaScript.
-			wp_localize_script(
-				'simple_history_script',
-				'simpleHistoryScriptVars',
-				array(
-					'settingsConfirmClearLog' => __( 'Remove all log items?', 'simple-history' ),
-				)
-			);
-
-			// Call plugins admin_css-method, so they can add CSS.
-			foreach ( $this->simple_history->get_instantiated_loggers() as $one_logger ) {
-				$one_logger['instance']->admin_css();
-			}
-
-			/**
-			 * Fires when the admin scripts have been enqueued.
-			 * Only fires on any of the pages where Simple History is used.
-			 *
-			 * @since 2.0
-			 *
-			 * @param Simple_History $instance The Simple_History instance.
-			 */
-			do_action( 'simple_history/enqueue_admin_scripts', $this->simple_history );
+		// Bail if not on our own pages.
+		if ( ! Helpers::is_on_our_own_pages() ) {
+			return;
 		}
+
+		add_thickbox();
+
+		wp_enqueue_style(
+			'simple_history_styles',
+			SIMPLE_HISTORY_DIR_URL . 'css/styles.css',
+			false,
+			SIMPLE_HISTORY_VERSION
+		);
+
+		wp_enqueue_style(
+			'simple_history_utility_styles',
+			SIMPLE_HISTORY_DIR_URL . 'css/utility-classes.css',
+			false,
+			SIMPLE_HISTORY_VERSION
+		);
+
+		wp_enqueue_script(
+			'simple_history_script',
+			SIMPLE_HISTORY_DIR_URL . 'js/scripts.js',
+			array( 'jquery', 'backbone', 'wp-util' ),
+			SIMPLE_HISTORY_VERSION,
+			true
+		);
+
+		// Translations that we use in JavaScript.
+		wp_localize_script(
+			'simple_history_script',
+			'simpleHistoryScriptVars',
+			array(
+				'settingsConfirmClearLog' => __( 'Remove all log items?', 'simple-history' ),
+			)
+		);
+
+		// Call plugins admin_css-method, so they can add CSS.
+		foreach ( $this->simple_history->get_instantiated_loggers() as $one_logger ) {
+			$one_logger['instance']->admin_css();
+		}
+
+		/**
+		 * Fires when the admin scripts have been enqueued.
+		 * Only fires on any of the pages where Simple History is used.
+		 *
+		 * @since 2.0
+		 *
+		 * @param Simple_History $instance The Simple_History instance.
+		 */
+		do_action( 'simple_history/enqueue_admin_scripts', $this->simple_history );
+	
 	}
 }
