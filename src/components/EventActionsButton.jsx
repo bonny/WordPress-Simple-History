7966bad7113698cfa2960bcc92792fd1ee915343
Pär Thernström <par.thernstrom@gmail.com>
2025-05-02 11:55:14 +0200

    Move similar events component to own file

--- src/components/EventActionsButton.jsx
+++ src/components/EventActionsButton.jsx
@@ -1,9 +1,9 @@
-import { DropdownMenu, MenuGroup, MenuItem, Slot } from '@wordpress/components';
+import { DropdownMenu, MenuGroup, Slot } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { moreHorizontalMobile, search } from '@wordpress/icons';
-import { addQueryArgs } from '@wordpress/url';
+import { moreHorizontalMobile } from '@wordpress/icons';
 import { EventCopyLinkMenuItem } from './EventCopyLinkMenuItem';
 import { EventDetailsMenuItem } from './EventDetailsMenuItem';
+import { EventViewMoreSimilarEventsMenuItem } from './EventViewMoreSimilarEventsMenuItem';
 
 /**
  * Add menu item that let user view more results from
@@ -20,74 +20,6 @@ import { EventDetailsMenuItem } from './EventDetailsMenuItem';
  * @param {Object} props.event
  * @param {string} props.eventsAdminPageURL URL to the events admin page
  */
-function EventViewMoreSimilarEventsMenuItem( { event, eventsAdminPageURL } ) {
-	const isWPUserInitiatorWithIdAndEmail =
-		event?.initiator === 'wp_user' &&
-		event?.initiator_data?.user_id &&
-		event?.initiator_data?.user_email;
-
-	const isLoggerAndMessageEvent = event?.logger && event?.message_key;
-
-	return (
-		<>
-			{ isWPUserInitiatorWithIdAndEmail ? (
-				<MenuItem
-					icon={ search }
-					iconPosition="left"
-					onClick={ () => {
-						// Example URL when searching for user, where user key is an array of objects with id and value keys.
-						// /wp-admin/admin.php?page=simple_history_admin_menu_page&users=[{%22id%22:%221%22,%22value%22:%22P%C3%A4r+(par@earthpeople.se)%22}]
-						const userJsonString = JSON.stringify( [
-							{
-								id: event.initiator_data.user_id,
-								value: event.initiator_data.user_email,
-							},
-						] );
-						const viewUserEventsURL = addQueryArgs(
-							eventsAdminPageURL,
-							{
-								users: userJsonString,
-							}
-						);
-						window.location.href = viewUserEventsURL;
-					} }
-				>
-					{ __( 'Find events by the same user', 'simple-history' ) }
-				</MenuItem>
-			) : null }
-
-			{ isLoggerAndMessageEvent ? (
-				<MenuItem
-					icon={ search }
-					iconPosition="left"
-					onClick={ () => {
-						// /wp-admin/admin.php?page=simple_history_admin_menu_page&messages=[{"value":"+-+All+found+updates","search_options":["AvailableUpdatesLogger:core_update_available","AvailableUpdatesLogger:plugin_update_available","AvailableUpdatesLogger:theme_update_available"]}]
-						const messageJsonString = JSON.stringify( [
-							{
-								value: event.message_key,
-								search_options: [
-									`${ event.logger }:${ event.message_key }`,
-								],
-							},
-						] );
-						const viewUserEventsURL = addQueryArgs(
-							eventsAdminPageURL,
-							{
-								messages: messageJsonString,
-							}
-						);
-						window.location.href = viewUserEventsURL;
-					} }
-				>
-					{ __(
-						'Filter event by this event type',
-						'simple-history'
-					) }
-				</MenuItem>
-			) : null }
-		</>
-	);
-}
 
 /**
  * The button with three dots that opens a dropdown with actions for the event.
---
+++ src/components/EventViewMoreSimilarEventsMenuItem.jsx
@@ -0,0 +1,91 @@
+import { MenuItem } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { search } from '@wordpress/icons';
+import { addQueryArgs } from '@wordpress/url';
+
+/**
+ * Add menu item that let user view more results from
+ *
+ * Can add now:
+ * - This user
+ * - Logger and message
+ *
+ * Can add in the future, when support is added to search filter:
+ *   - Initiator
+ *   - IP address
+ *
+ * @param {Object} props
+ * @param {Object} props.event
+ * @param {string} props.eventsAdminPageURL URL to the events admin page
+ */
+export function EventViewMoreSimilarEventsMenuItem( {
+	event,
+	eventsAdminPageURL,
+} ) {
+	const isWPUserInitiatorWithIdAndEmail =
+		event?.initiator === 'wp_user' &&
+		event?.initiator_data?.user_id &&
+		event?.initiator_data?.user_email;
+
+	const isLoggerAndMessageEvent = event?.logger && event?.message_key;
+
+	return (
+		<>
+			{ isWPUserInitiatorWithIdAndEmail ? (
+				<MenuItem
+					icon={ search }
+					iconPosition="left"
+					onClick={ () => {
+						// Example URL when searching for user, where user key is an array of objects with id and value keys.
+						// /wp-admin/admin.php?page=simple_history_admin_menu_page&users=[{%22id%22:%221%22,%22value%22:%22P%C3%A4r+(par@earthpeople.se)%22}]
+						const userJsonString = JSON.stringify( [
+							{
+								id: event.initiator_data.user_id,
+								value: event.initiator_data.user_email,
+							},
+						] );
+						const viewUserEventsURL = addQueryArgs(
+							eventsAdminPageURL,
+							{
+								users: userJsonString,
+							}
+						);
+						window.location.href = viewUserEventsURL;
+					} }
+				>
+					{ __( 'Find events by the same user', 'simple-history' ) }
+				</MenuItem>
+			) : null }
+
+			{ isLoggerAndMessageEvent ? (
+				<MenuItem
+					icon={ search }
+					iconPosition="left"
+					onClick={ () => {
+						// /wp-admin/admin.php?page=simple_history_admin_menu_page&messages=[{"value":"+-+All+found+updates","search_options":["AvailableUpdatesLogger:core_update_available","AvailableUpdatesLogger:plugin_update_available","AvailableUpdatesLogger:theme_update_available"]}]
+						const messageJsonString = JSON.stringify( [
+							{
+								value: event.message_key,
+								search_options: [
+									`${ event.logger }:${ event.message_key }`,
+								],
+							},
+						] );
+						const viewUserEventsURL = addQueryArgs(
+							eventsAdminPageURL,
+							{
+								messages: messageJsonString,
+							}
+						);
+						window.location.href = viewUserEventsURL;
+					} }
+				>
+					{ __(
+						'Filter event by this event type',
+						'simple-history'
+					) }
+				</MenuItem>
+			) : null }
+		</>
+	);
+}
