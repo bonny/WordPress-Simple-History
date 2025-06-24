a29f16bd202ba89e12526d64ab08998f24875aa4
Pär Thernström <par.thernstrom@gmail.com>
2025-06-24 20:03:24 +0200

    Line

--- templates/email-summary-report.php
+++ templates/email-summary-report.php
@@ -277,7 +277,9 @@ $content_after_core_stats = apply_filters( 'simple_history/email_summary_report/
 					</p>
 					<p style="margin: 10px 0 0; font-size: 12px; color: #000000;">
 						<?php echo esc_html( __( 'This email was auto-generated and sent from www.simple-history.com.', 'simple-history' ) ); ?>
-						<a href="<?php echo esc_url( $learn_how_to_unsubscribe_url ); ?>" style="color: #000000; text-decoration: underline;"><?php echo esc_html( __( 'Learn how to unsubscribe/stop receiving emails', 'simple-history' ) ); ?></a>.
+						<a href="<?php echo esc_url( $learn_how_to_unsubscribe_url ); ?>" style="color: #000000; text-decoration: underline;">
+							<?php echo esc_html( __( 'Learn how to unsubscribe/stop receiving emails', 'simple-history' ) ); ?>
+						</a>.
 					</p>
 				</td>
 			</tr>
