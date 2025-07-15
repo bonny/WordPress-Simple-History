215a944fc38cfc6bff02596e1533f93f0a62def5
Pär Thernström <par.thernstrom@gmail.com>
2025-07-15 11:12:22 +0200

    Update example dropin

--- tests/_data/mu-plugins/inc/class-example-dropin.php
+++ tests/_data/mu-plugins/inc/class-example-dropin.php
@@ -2,9 +2,12 @@
 
 /**
  * This is the class that does the main work!
+ * 
+ * This is the old way, using register settings tab.
+ * Should be updated.
  */
 class Example_Dropin {
-	// This will hold a reference to the simple history instance.
+	/** @var \Simple_History\Simple_History This will hold a reference to the simple history instance. */
 	private $sh;
 
 	// Simple History will pass itself to the constructor.
@@ -18,7 +21,7 @@ class Example_Dropin {
 	}
 
 	public function add_settings_tab() {
-		$this->sh->registerSettingsTab(
+		$this->sh->register_settings_tab(
 			array(
 				'slug' => 'dropin_example_tab_slug',
 				'name' => __( 'Dropin example tab', 'simple-history' ),
