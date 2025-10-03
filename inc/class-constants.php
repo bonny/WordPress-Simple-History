3974cec01ba2ee2ddc9659ffa00bafeb3b1b1a5f
Pär Thernström <par.thernstrom@gmail.com>
2025-10-03 19:17:53 +0200

    Add more helper functions to constants class

--- inc/class-constants.php
+++ inc/class-constants.php
@@ -59,4 +59,119 @@ class Constants {
 			'quarter'   => self::DAYS_PER_QUARTER,
 		);
 	}
+
+	/**
+	 * Get current timestamp.
+	 *
+	 * @return int Current Unix timestamp.
+	 */
+	public static function get_current_timestamp() {
+		return time();
+	}
+
+	/**
+	 * Get timestamp for start of today (00:00:00) in WordPress timezone.
+	 *
+	 * Uses WordPress timezone setting from Settings > General.
+	 *
+	 * @return int Unix timestamp for start of today.
+	 */
+	public static function get_today_start_timestamp() {
+		$today = new \DateTimeImmutable( 'today', wp_timezone() );
+		return $today->getTimestamp();
+	}
+
+	/**
+	 * Get timestamp for end of today (23:59:59) in WordPress timezone.
+	 *
+	 * Uses WordPress timezone setting from Settings > General.
+	 *
+	 * @return int Unix timestamp for end of today.
+	 */
+	public static function get_today_end_timestamp() {
+		$today_end = new \DateTimeImmutable( 'today 23:59:59', wp_timezone() );
+		return $today_end->getTimestamp();
+	}
+
+	/**
+	 * Get timestamp for N days ago (00:00:00) in WordPress timezone.
+	 *
+	 * Uses WordPress timezone setting from Settings > General.
+	 *
+	 * @param int $days Number of days to go back.
+	 * @return int Unix timestamp for start of day N days ago.
+	 */
+	public static function get_n_days_ago_timestamp( $days ) {
+		$date = new \DateTimeImmutable( "-{$days} days", wp_timezone() );
+		$date_start = new \DateTimeImmutable( $date->format( 'Y-m-d' ) . ' 00:00:00', wp_timezone() );
+		return $date_start->getTimestamp();
+	}
+
+	/**
+	 * Get default date range (last 30 days to end of today).
+	 *
+	 * Uses WordPress timezone setting from Settings > General.
+	 *
+	 * @return array Array with 'from' and 'to' Unix timestamps.
+	 */
+	public static function get_default_date_range() {
+		return array(
+			'from' => self::get_n_days_ago_timestamp( self::DAYS_PER_MONTH ),
+			'to'   => self::get_today_end_timestamp(),
+		);
+	}
+
+	/**
+	 * Get date range for last N days.
+	 *
+	 * Range goes from N days ago (00:00:00) to end of today (23:59:59).
+	 * Uses WordPress timezone setting from Settings > General.
+	 *
+	 * @param int $days Number of days.
+	 * @return array Array with 'from' and 'to' Unix timestamps.
+	 */
+	public static function get_last_n_days_range( $days ) {
+		return array(
+			'from' => self::get_n_days_ago_timestamp( $days ),
+			'to'   => self::get_today_end_timestamp(),
+		);
+	}
+
+	/**
+	 * Get date range for a specific period (week, month, fortnight, quarter).
+	 *
+	 * Range goes from period start (00:00:00) to end of today (23:59:59).
+	 * Uses WordPress timezone setting from Settings > General.
+	 *
+	 * @param string $period Period name: 'week', 'month', 'fortnight', 'quarter'.
+	 * @return array Array with 'from' and 'to' Unix timestamps.
+	 */
+	public static function get_period_range( $period ) {
+		$days = self::get_days_for_period( $period );
+		return self::get_last_n_days_range( $days );
+	}
+
+	/**
+	 * Get WordPress timezone object.
+	 *
+	 * Returns DateTimeZone object for the site's timezone as configured
+	 * in Settings > General.
+	 *
+	 * @return \DateTimeZone WordPress site timezone.
+	 */
+	public static function get_wp_timezone() {
+		return wp_timezone();
+	}
+
+	/**
+	 * Get WordPress timezone string.
+	 *
+	 * Returns timezone string like 'Europe/Stockholm' or '+02:00'.
+	 * Uses Settings > General timezone configuration.
+	 *
+	 * @return string Timezone string.
+	 */
+	public static function get_wp_timezone_string() {
+		return wp_timezone_string();
+	}
 }
