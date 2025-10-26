00a99fc2f44253bfafb74b5da3a8c952aa5491bb
Pär Thernström <par.thernstrom@gmail.com>
2025-10-26 11:23:13 +0100

    Use wpdb->prepare in tests instead of using sprintf

--- tests/wpunit/functions.php
+++ tests/wpunit/functions.php
@@ -5,6 +5,8 @@ namespace Simple_History\tests;
 use Simple_History\Simple_History;
 
 /**
+ * Get the latest row from the events table, date or contents does not matter.
+ * 
  * @param bool $unset_fields Unset some fields that are different on each run, making it difficult to compare.
  * @return array
  */
@@ -21,13 +23,24 @@ function get_latest_row( $unset_fields = true ) {
 	return $latest_row;
 }
 
+/**
+ * Get the latest context from the contexts table.
+ * 
+ * @param bool $unset_fields Unset some fields that are different on each run, making it difficult to compare.
+ * @return array
+ */
 function get_latest_context( $unset_fields = true ) {
 	$latest_row = get_latest_row( false );
 
 	global $wpdb;
 	$db_table_contexts = Simple_History::get_instance()->get_contexts_table_name();
-	// // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
-	$latest_context = $wpdb->get_results( "SELECT * FROM {$db_table_contexts} WHERE history_id = {$latest_row['id']} ORDER BY `key` ASC", ARRAY_A );
+	$latest_context = $wpdb->get_results(
+		$wpdb->prepare(
+			"SELECT * FROM {$db_table_contexts} WHERE history_id = %d ORDER BY `key` ASC",
+			$latest_row['id']
+		),
+		ARRAY_A
+	);
 
 	if ( $unset_fields ) {
 		$latest_context = array_map(
