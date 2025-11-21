cc3d14cf7c1b65f5da4230c13a393d4830922b4b
Pär Thernström <par.thernstrom@gmail.com>
2025-11-21 15:08:52 +0100

    Update experimental features title

--- inc/services/class-experimental-features-page.php
+++ inc/services/class-experimental-features-page.php
@@ -52,7 +52,7 @@ class Experimental_Features_Page extends Service {
 				->set_location( 'submenu' );
 		} else {
 			$experimental_page
-				->set_menu_title( __( 'Simple History - Experimental', 'simple-history' ) )
+				->set_menu_title( __( 'Experimental features', 'simple-history' ) )
 				->set_parent( Simple_History::SETTINGS_MENU_PAGE_SLUG )
 				->set_location( 'submenu' );
 		}
